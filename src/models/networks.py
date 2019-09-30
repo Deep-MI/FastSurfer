d9a527994345934e1212c3ab80758955c60ae6df
LeHenschel <leonie.henschel@rwth-aachen.de>
2019-09-30 18:43:51 +0200

    Final version of FastSurferCNN

---
+++ src/models/networks.py
@@ -0,0 +1,67 @@
+# IMPORTS
+import torch.nn as nn
+import models.sub_module as sm
+
+
+class FastSurferCNN(nn.Module):
+    """
+    Network Definition of Fully Competitive Network network
+    * Spatial view aggregation (input 7 slices of which only middle one gets segmented)
+    * Same Number of filters per layer (normally 64)
+    * Dense Connections in blocks
+    * Unpooling instead of transpose convolutions
+    * Concatenationes are replaced with Maxout (competitive dense blocks)
+    * Global skip connections are fused by Maxout (global competition)
+    * Loss Function (weighted Cross-Entropy and dice loss)
+    """
+    def __init__(self, params):
+        super(FastSurferCNN, self).__init__()
+
+        # Parameters for the Descending Arm
+        self.encode1 = sm.CompetitiveEncoderBlockInput(params)
+        params['num_channels'] = params['num_filters']
+        self.encode2 = sm.CompetitiveEncoderBlock(params)
+        self.encode3 = sm.CompetitiveEncoderBlock(params)
+        self.encode4 = sm.CompetitiveEncoderBlock(params)
+        self.bottleneck = sm.CompetitiveDenseBlock(params)
+
+        # Parameters for the Ascending Arm
+        params['num_channels'] = params['num_filters']
+        self.decode4 = sm.CompetitiveDecoderBlock(params)
+        self.decode3 = sm.CompetitiveDecoderBlock(params)
+        self.decode2 = sm.CompetitiveDecoderBlock(params)
+        self.decode1 = sm.CompetitiveDecoderBlock(params)
+
+        params['num_channels'] = params['num_filters']
+        self.classifier = sm.ClassifierBlock(params)
+
+        # Code for Network Initialization
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='leaky_relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        """
+        Computational graph
+        :param tensor x: input image
+        :return tensor: prediction logits
+        """
+        encoder_output1, skip_encoder_1, indices_1 = self.encode1.forward(x)
+        encoder_output2, skip_encoder_2, indices_2 = self.encode2.forward(encoder_output1)
+        encoder_output3, skip_encoder_3, indices_3 = self.encode3.forward(encoder_output2)
+        encoder_output4, skip_encoder_4, indices_4 = self.encode4.forward(encoder_output3)
+
+        bottleneck = self.bottleneck(encoder_output4)
+
+        decoder_output4 = self.decode4.forward(bottleneck, skip_encoder_4, indices_4)
+        decoder_output3 = self.decode3.forward(decoder_output4, skip_encoder_3, indices_3)
+        decoder_output2 = self.decode2.forward(decoder_output3, skip_encoder_2, indices_2)
+        decoder_output1 = self.decode1.forward(decoder_output2, skip_encoder_1, indices_1)
+
+        logits = self.classifier.forward(decoder_output1)
+
+        return logits
