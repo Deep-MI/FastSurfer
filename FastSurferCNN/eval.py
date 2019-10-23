010290a538d18e8946cedce6c423c18952ffa65c
LeHenschel <leonie.henschel@rwth-aachen.de>
2019-10-23 11:43:29 +0200

    Updated license information

--- FastSurferCNN/eval.py
+++ FastSurferCNN/eval.py
@@ -1,5 +1,5 @@
 
-# Copyright 2019 Image Analysis Lab, German Center for Neurodegenrative Diseases (DZNE), Bonn
+# Copyright 2019 Image Analysis Lab, German Center for Neurodegenerative Diseases (DZNE), Bonn
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
