21c7c0579061ce0787c4d73ce74fad394d0d2f11
LeHenschel <leonie.henschel@rwth-aachen.de>
2019-11-19 15:12:09 +0100

    Update for FreeSurfer dev version support - fix fo reading surface files with nibabel and binpath inside recon-surf

--- recon_surf/spherically_project.py
+++ recon_surf/spherically_project.py
@@ -1,4 +1,3 @@
-
 # Copyright 2019 Image Analysis Lab, German Center for Neurodegenerative Diseases (DZNE), Bonn
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,13 +15,12 @@
 
 # IMPORTS
 import optparse
-import os
 import sys
 import nibabel.freesurfer.io as fs
 import numpy as np
 import math
 from scipy import sparse
-from scipy.sparse.linalg import eigsh
+from read_geometry import read_geometry
 
 HELPTEXT = """
 Script to compute ShapeDNA using linear FEM matrices. 
@@ -194,7 +192,7 @@ def laplaceTria(v, t, k=10):
     return eigenvalues, eigenvectors
 
 
-def exportEV(d,outfile):
+def exportEV(d, outfile):
     """
     Save EV file
     
@@ -204,65 +202,65 @@ def exportEV(d,outfile):
 
     # open file
     try:
-        f = open(outfile,'w')
+        f = open(outfile, 'w')
     except IOError:
-        print("[File "+outfile+" not writable]")
+        print("[File " + outfile + " not writable]")
         return
 
     # check data structure
     if not 'Eigenvalues' in d:
         print("ERROR: no Eigenvalues specified")
         exit(1)
-    
+
     # ...
 
     #
-    if 'Creator' in d: f.write(' Creator: '+d['Creator']+'\n')
-    if 'File' in d: f.write(' File: '+d['File']+'\n')
-    if 'User' in d: f.write(' User: '+d['User']+'\n')
-    if 'Refine' in d: f.write(' Refine: '+str(d['Refine'])+'\n')
-    if 'Degree' in d: f.write(' Degree: '+str(d['Degree'])+'\n')
-    if 'Dimension' in d: f.write(' Dimension: '+str(d['Dimension'])+'\n')
-    if 'Elements' in d: f.write(' Elements: '+str(d['Elements'])+'\n')
-    if 'DoF' in d: f.write(' DoF: '+str(d['DoF'])+'\n')
-    if 'NumEW' in d: f.write(' NumEW: '+str(d['NumEW'])+'\n')
+    if 'Creator' in d: f.write(' Creator: ' + d['Creator'] + '\n')
+    if 'File' in d: f.write(' File: ' + d['File'] + '\n')
+    if 'User' in d: f.write(' User: ' + d['User'] + '\n')
+    if 'Refine' in d: f.write(' Refine: ' + str(d['Refine']) + '\n')
+    if 'Degree' in d: f.write(' Degree: ' + str(d['Degree']) + '\n')
+    if 'Dimension' in d: f.write(' Dimension: ' + str(d['Dimension']) + '\n')
+    if 'Elements' in d: f.write(' Elements: ' + str(d['Elements']) + '\n')
+    if 'DoF' in d: f.write(' DoF: ' + str(d['DoF']) + '\n')
+    if 'NumEW' in d: f.write(' NumEW: ' + str(d['NumEW']) + '\n')
     f.write('\n')
-    if 'Area' in d: f.write(' Area: '+str(d['Area'])+'\n')    
-    if 'Volume' in d: f.write(' Volume: '+str(d['Volume'])+'\n')
-    if 'BLength' in d: f.write(' BLength: '+str(d['BLength'])+'\n')
-    if 'EulerChar' in d: f.write(' EulerChar: '+str(d['EulerChar'])+'\n')
+    if 'Area' in d: f.write(' Area: ' + str(d['Area']) + '\n')
+    if 'Volume' in d: f.write(' Volume: ' + str(d['Volume']) + '\n')
+    if 'BLength' in d: f.write(' BLength: ' + str(d['BLength']) + '\n')
+    if 'EulerChar' in d: f.write(' EulerChar: ' + str(d['EulerChar']) + '\n')
     f.write('\n')
-    if 'TimePre' in d: f.write(' Time(Pre) : '+str(d['TimePre'])+'\n')
-    if 'TimeCalcAB' in d: f.write(' Time(calcAB) : '+str(d['TimeCalcAB'])+'\n')
-    if 'TimeCalcEW' in d: f.write(' Time(calcEW) : '+str(d['TimeCalcEW'])+'\n')
+    if 'TimePre' in d: f.write(' Time(Pre) : ' + str(d['TimePre']) + '\n')
+    if 'TimeCalcAB' in d: f.write(' Time(calcAB) : ' + str(d['TimeCalcAB']) + '\n')
+    if 'TimeCalcEW' in d: f.write(' Time(calcEW) : ' + str(d['TimeCalcEW']) + '\n')
     if 'TimePre' in d and 'TimeCalcAB' in d and 'TimeCalcEW' in d:
-        f.write(' Time(total ) : '+str(d['TimePre']+d['TimeCalcAB']+d['TimeCalcEW'])+'\n')
+        f.write(' Time(total ) : ' + str(d['TimePre'] + d['TimeCalcAB'] + d['TimeCalcEW']) + '\n')
 
     f.write('\n')
     f.write('Eigenvalues:\n')
-    f.write('{ '+' ; '.join(map(str,d['Eigenvalues']))+' }\n') # consider precision
+    f.write('{ ' + ' ; '.join(map(str, d['Eigenvalues'])) + ' }\n')  # consider precision
     f.write('\n')
-    
+
     if 'Eigenvectors' in d:
         f.write('Eigenvectors:\n')
-        #f.write('sizes: '+' '.join(map(str,d['EigenvectorsSize']))+'\n')
-	# better compute real sizes from eigenvector array?
-        f.write('sizes: '+' '.join(map(str,d['Eigenvectors'].shape))+'\n')
+        # f.write('sizes: '+' '.join(map(str,d['EigenvectorsSize']))+'\n')
+        # better compute real sizes from eigenvector array?
+        f.write('sizes: ' + ' '.join(map(str, d['Eigenvectors'].shape)) + '\n')
         f.write('\n')
         f.write('{ ')
-        for i in range(np.shape(d['Eigenvectors'])[1]-1):
+        for i in range(np.shape(d['Eigenvectors'])[1] - 1):
             f.write('(')
-            f.write(','.join(map(str,d['Eigenvectors'][:,i])))
+            f.write(','.join(map(str, d['Eigenvectors'][:, i])))
             f.write(') ;\n')
         f.write('(')
-        f.write(','.join(map(str,d['Eigenvectors'][:,np.shape(d['Eigenvectors'])[1]-1])))
+        f.write(','.join(map(str, d['Eigenvectors'][:, np.shape(d['Eigenvectors'])[1] - 1])))
         f.write(') }\n')
 
     # close file
     f.close()
 
 
-def get_tria_areas(v,t):
+def get_tria_areas(v, t):
     """
     Computes the surface area of triangles using cross product of edges.
     
@@ -348,16 +346,16 @@ def sphericalProject(v, t):
               t - triangles: same as before
 
     """
-    
-    #print("Tria min vidx: {}  max vidx: {}".format(np.min(t), np.max(t)))
-    #print("Tria range: {}".format(np.max(t)- np.min(t)+1))
-    #print("Vnumber: {}".format(v.shape[0]))
-    
+
+    # print("Tria min vidx: {}  max vidx: {}".format(np.min(t), np.max(t)))
+    # print("Tria range: {}".format(np.max(t)- np.min(t)+1))
+    # print("Vnumber: {}".format(v.shape[0]))
+
     evals, evecs = laplaceTria(v, t, k=4)
-    
-    #debug=True
-    debug=False
-    
+
+    # debug=True
+    debug = False
+
     if debug:
         data = dict()
         data['Eigenvalues'] = evals
@@ -369,72 +367,71 @@ def sphericalProject(v, t):
         data['Elements'] = t.shape[0]
         data['DoF'] = evecs.shape[0]
         data['NumEW'] = 4
-        exportEV(data,'debug.ev')
-
+        exportEV(data, 'debug.ev')
 
     # flip efuncs to align to coordinates consistently
-    ev1 = evecs[:,1]
-    #ev1maxi = np.argmax(ev1)
-    #ev1mini = np.argmin(ev1)
-    #cmax = v[ev1maxi,:]
-    #cmin = v[ev1mini,:]
-    cmax1 = np.mean(v[ev1>0.5*np.max(ev1),:],0)
-    cmin1 = np.mean(v[ev1<0.5*np.min(ev1),:],0)
-    ev2 = evecs[:,2]
-    cmax2 = np.mean(v[ev2>0.5*np.max(ev2),:],0)
-    cmin2 = np.mean(v[ev2<0.5*np.min(ev2),:],0)
-    ev3 = evecs[:,3]
-    cmax3 = np.mean(v[ev3>0.5*np.max(ev3),:],0)
-    cmin3 = np.mean(v[ev3<0.5*np.min(ev3),:],0)
-    
+    ev1 = evecs[:, 1]
+    # ev1maxi = np.argmax(ev1)
+    # ev1mini = np.argmin(ev1)
+    # cmax = v[ev1maxi,:]
+    # cmin = v[ev1mini,:]
+    cmax1 = np.mean(v[ev1 > 0.5 * np.max(ev1), :], 0)
+    cmin1 = np.mean(v[ev1 < 0.5 * np.min(ev1), :], 0)
+    ev2 = evecs[:, 2]
+    cmax2 = np.mean(v[ev2 > 0.5 * np.max(ev2), :], 0)
+    cmin2 = np.mean(v[ev2 < 0.5 * np.min(ev2), :], 0)
+    ev3 = evecs[:, 3]
+    cmax3 = np.mean(v[ev3 > 0.5 * np.max(ev3), :], 0)
+    cmin3 = np.mean(v[ev3 < 0.5 * np.min(ev3), :], 0)
+
     # we trust ev 1 goes from front to back
-    l11 = abs(cmax1[1]-cmin1[1])
-    l21 = abs(cmax2[1]-cmin2[1])
-    l31 = abs(cmax3[1]-cmin3[1])
-    if (l11 < l21 or l11 < l31):
+    l11 = abs(cmax1[1] - cmin1[1])
+    l21 = abs(cmax2[1] - cmin2[1])
+    l31 = abs(cmax3[1] - cmin3[1])
+    if l11 < l21 or l11 < l31:
         print("ERROR: direction 1 should be (anterior -posterior) but is not!")
         print("  debug info: {} {} {} ".format(l11, l21, l31))
         sys.exit(1)
-	
+
     # only flip direction if necessary
-    print("ev1 min: {}  max {} ".format(cmin1,cmax1))
+    print("ev1 min: {}  max {} ".format(cmin1, cmax1))
     # axis 1 = y is aligned with this function (for brains in FS space)
     v1 = cmax1 - cmin1
-    if (cmax1[1] < cmin1[1]):
+    if cmax1[1] < cmin1[1]:
         ev1 = -1 * ev1;
         print("inverting direction 1 (anterior - posterior)")
-    l1 = abs(cmax1[1]-cmin1[1])
-    
+    l1 = abs(cmax1[1] - cmin1[1])
+
     # for ev2 and ev3 there could be also a swap of the two
-    l22 = abs(cmax2[2]-cmin2[2])
-    l32 = abs(cmax3[2]-cmin3[2])
-    #usually ev2 should be superior inferior, if ev3 is better in that direction, swap
-    if ( l22 < l32 ):
+    l22 = abs(cmax2[2] - cmin2[2])
+    l32 = abs(cmax3[2] - cmin3[2])
+    # usually ev2 should be superior inferior, if ev3 is better in that direction, swap
+    if l22 < l32:
         print("swapping direction 2 and 3")
         ev2, ev3 = ev3, ev2
         cmax2, cmax3 = cmax3, cmax2
         cmin2, cmin3 = cmin3, cmin2
-    l23 = abs(cmax2[0]-cmin2[0])
-    l33 = abs(cmax3[0]-cmin3[0])
-    if ( l33 < l23 ): 
+    l23 = abs(cmax2[0] - cmin2[0])
+    l33 = abs(cmax3[0] - cmin3[0])
+    if l33 < l23:
         print("WARNING: direction 3 wants to swap with 2, but cannot")
-    
-    print("ev2 min: {}  max {} ".format(cmin2,cmax2))
+
+    print("ev2 min: {}  max {} ".format(cmin2, cmax2))
     # axis 2 = z is aligned with this function (for brains in FS space)
     v2 = cmax2 - cmin2
-    if (cmax2[2] < cmin2[2]):
+    if cmax2[2] < cmin2[2]:
         ev2 = -1 * ev2;
         print("inverting direction 2 (superior - inferior)")
-    l2 = abs(cmax2[2]-cmin2[2])
-        
-    print("ev3 min: {}  max {} ".format(cmin3,cmax3))
+    l2 = abs(cmax2[2] - cmin2[2])
+
+    print("ev3 min: {}  max {} ".format(cmin3, cmax3))
     # axis 0 = x is aligned with this function (for brains in FS space)
     v3 = cmax3 - cmin3
-    if (cmax3[0] < cmin3[0]):
+    if cmax3[0] < cmin3[0]:
         ev3 = -1 * ev3;
         print("inverting direction 3 (right - left)")
     l3 = abs(cmax3[0] - cmin3[0])
-    
+
     v1 = v1 * (1.0 / np.sqrt(np.sum(v1 * v1)))
     v2 = v2 * (1.0 / np.sqrt(np.sum(v2 * v2)))
     v3 = v3 * (1.0 / np.sqrt(np.sum(v3 * v3)))
@@ -465,7 +462,6 @@ def sphericalProject(v, t):
     ev3[ev3 < 0] /= - ev3min
     ev3[ev3 > 0] /= ev3max
 
-
     # project to sphere and scaled to have the same scale/origin as FS:
     dist = np.sqrt(np.square(ev1) + np.square(ev2) + np.square(ev3))
     v[:, 0] = 100 * (ev3 / dist)
@@ -476,15 +472,15 @@ def sphericalProject(v, t):
     print("sphere area fraction: {} ".format(svol))
 
     flippedarea = get_flipped_area(v, t) / (4.0 * math.pi * 10000)
-    if (flippedarea > 0.95):
+    if flippedarea > 0.95:
         print("ERROR: global normal flip, exiting ..")
         sys.exit(1)
-        #print("WARNING: flipping normals globally!")
-        #tt = np.copy(t[:,1])
-        #t[:,1] = np.copy(t[:,2])
-        #t[:,2] = tt
-        #flippedarea=get_flipped_area(v,t)/(4.0*math.pi*10000)
-    
+        # print("WARNING: flipping normals globally!")
+        # tt = np.copy(t[:,1])
+        # t[:,1] = np.copy(t[:,2])
+        # t[:,2] = tt
+        # flippedarea=get_flipped_area(v,t)/(4.0*math.pi*10000)
+
     print("flipped area fraction: {} ".format(flippedarea))
 
     if svol < 0.99:
@@ -509,7 +505,7 @@ def spherically_project_surface(insurf, outsurf):
     """ (string) -> None
     takes path to insurf, spherically projects it, outputs it to outsurf
     """
-    surf = fs.read_geometry(insurf, read_metadata=True)
+    surf = read_geometry(insurf, read_metadata=True)
     projected = sphericalProject(surf[0], surf[1])
     fs.write_geometry(outsurf, projected[0], projected[1], volume_info=surf[2])
 
