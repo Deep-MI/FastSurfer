4d55fa3e60257dd7dc01deb9df3bb3c1c9e94904
LeHenschel <leonie.henschel@rwth-aachen.de>
2019-11-28 16:48:57 +0100

    Fixed shebang error (not at top of file)

--- run_fastsurfer.sh
+++ run_fastsurfer.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 # Copyright 2019 Image Analysis Lab, German Center for Neurodegenerative Diseases (DZNE), Bonn
 #
@@ -13,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-#!/bin/bash
 
 VERSION='$Id$'
 
