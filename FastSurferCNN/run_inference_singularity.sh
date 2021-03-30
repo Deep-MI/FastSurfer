ed3d7852a6ad2c200f37c9d92056fae6a8d63396
Jérôme Dockès <jdockes@beluga1.int.ets1.calculquebec.ca>
2021-03-30 19:03:24 -0400

    start preparing for ukb on beluga

---
+++ FastSurferCNN/run_inference_singularity.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --nodes 1          # Request 1 nodes so all resources are in two nodes.
+#SBATCH --gres=gpu:1       # Request GPU "generic resources"
+#SBATCH --cpus-per-task=16  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=32G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --time=0-02:50
+#SBATCH --output=%N-%j.out
+
+# cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+module load singularity/3.6
+
+singularity exec --nv --overlay /project/rpp-aevans-ab/neurohub/ukbb/imaging/neurohub_ukbb_t1_ses2_0_bids.squashfs:ro ../ubuntu-python.sif ./run_inference_ukb.sh
--- FastSurferCNN/run_inference_ukb.sh
+++ FastSurferCNN/run_inference_ukb.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-python3 eval.py --i_dir /neurohub/ukbb/imaging/sub-2017717/ses-2/anat/ \
-	--o_dir ../data \
-	--t '*' \
-	--in_name sub-2017717_ses-2_T1w.nii.gz \
+DATA_DIR='/neurohub/ukbb/imaging/'
+INPUT_FILE_NAME='ses-2/anat/sub-2017717_ses-2_T1w.nii.gz'
+PARAM_T='sub-2017717'
+
+# cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+echo prune_percent 0.50 cohort NC
+python3 eval.py --i_dir "${DATA_DIR}" \
+	--o_dir ../data/prune_50/NC/ \
+	--t "${PARAM_T}" \
+	--in_name "${INPUT_FILE_NAME}" \
 	--log temp_Competitive.log \
 	--network_sagittal_path ../checkpoints/Sagittal_Weights_FastSurferCNN/ckpts/Epoch_30_training_state.pkl \
 	--network_coronal_path ../checkpoints/Coronal_Weights_FastSurferCNN/ckpts/Epoch_30_training_state.pkl \
 	--network_axial_path ../checkpoints/Axial_Weights_FastSurferCNN/ckpts/Epoch_30_training_state.pkl \
 	--prune_type layerwise \
-	--prune_percent 0.1
-
+	--prune_percent 0.5
