[run_prediction/eval/interpolation_layer] fix the memory issue, optimize view aggregation and interpolation layer, introduce half precision for view aggregation

FastSurferCNN/eval.py
@@ -121,7 +121,7 @@ class Inference:
         return self.device
 
     @torch.no_grad()
-    def eval(self, val_loader, pred_prob: torch.Tensor, out_scale=None):
+    def eval(self, val_loader, out: torch.Tensor, out_scale=None):
         """Perform prediction and inplace-aggregate views into pred_prob. Return pred_prob."""
         self.model.eval()
 
@@ -132,31 +132,31 @@ class Inference:
             pred = self.model(images, scale_factors, out_scale)
 
             # cut prediction to the image size
-            # TODO: a bit "hacky" to get the orig image size...; maybe this can just be in batch
+            # TODO: a bit "hacky" to get the orig image size of the current plane...; maybe this can just be in batch
             shape = val_loader.dataset.images.shape
             pred = pred[:, :, :shape[1], :shape[2]]
 
             if self.cfg.DATA.PLANE == "axial":
-                pred = pred.permute(3, 0, 2, 1).to(pred_prob.device)  # the to-operation is implicit
-                pred_prob[:, start_index:start_index + pred.shape[1], :, :].add_(pred, alpha=0.4)
+                pred = pred.permute(3, 0, 2, 1).to(out.device)  # the to-operation is implicit
+                out[:, start_index:start_index + pred.shape[1], :, :].add_(pred, alpha=0.4)
                 start_index += pred.shape[1]
 
             elif self.cfg.DATA.PLANE == "coronal":
-                pred = pred.permute(2, 3, 0, 1).to(pred_prob.device)
-                pred_prob[:, :, start_index:start_index + pred.shape[2], :].add_(pred, alpha=0.4)
+                pred = pred.permute(2, 3, 0, 1).to(out.device)
+                out[:, :, start_index:start_index + pred.shape[2], :].add_(pred, alpha=0.4)
                 start_index += pred.shape[2]
 
             else:
-                pred = map_prediction_sagittal2full(pred).permute(0, 3, 2, 1).to(pred_prob.device)
-                pred_prob[start_index:start_index + pred.shape[0], :, :, :].add_(pred, alpha=0.2)
+                pred = map_prediction_sagittal2full(pred).permute(0, 3, 2, 1).to(out.device)
+                out[start_index:start_index + pred.shape[0], :, :, :].add_(pred, alpha=0.2)
                 start_index += pred.shape[0]
 
             logger.info("---> Batch {} {} Testing Done.".format(batch_idx, self.cfg.DATA.PLANE))
 
-        return pred_prob
+        return out
 
     @torch.no_grad()
-    def run(self, img_filename, orig_data, orig_zoom, pred_prob, noise=0, out_res=None):
+    def run(self, img_filename, orig_data, orig_zoom, out, noise=0, out_res=None):
         # Set up DataLoader
         test_dataset = MultiScaleOrigDataThickSlices(img_filename, orig_data, orig_zoom, self.cfg, gn_noise=noise,
                                                      transforms=transforms.Compose([ToTensorTest()]))
@@ -168,8 +168,8 @@ class Inference:
 
         # Run evaluation
         start = time.time()
-        pred_prob = self.eval(test_data_loader, pred_prob, out_scale=out_res)
-        logger.info("Inference on {} finished in {:0.4f} seconds".format(img_filename, time.time()-start))
+        out = self.eval(test_data_loader, out, out_scale=out_res)
+        logger.info("{}-Inference on {} finished in {:0.4f} seconds".format(self.cfg.DATA.PLANE, img_filename, time.time()-start))
 
-        return pred_prob
+        return out
 

FastSurferCNN/models/interpolation_layer.py
@@ -16,6 +16,7 @@
 # IMPORTS
 import typing as _T
 from numbers import Number
+from logging import getLogger as _getLogger
 
 import torch
 import torch.nn as nn
@@ -23,6 +24,7 @@ from torch import Tensor
 from torch.nn import functional as _F
 import numpy as np
 
+LOGGER = _getLogger(__name__)
 
 T_Scale = _T.TypeVar('T_Scale', _T.List[float], Tensor)
 T_ScaleAll = _T.TypeVar('T_ScaleAll', _T.Sequence[float], Tensor, np.ndarray, float)
@@ -40,23 +42,30 @@ class _ZoomNd(nn.Module):
                                       (default: 'neareast')
         """
         super(_ZoomNd, self).__init__()
-        self._target_shape = target_shape
         self._mode = interpolation_mode
-        self._N = 0
+        if not hasattr(self, "_N"):
+            self._N = -1
+        self._target_shape: _T.Tuple[int, ...] = tuple()
+        self.target_shape = target_shape
 
-    def set_target_shape(self, target_shape: _T.Optional[_T.Sequence[int]]) -> None:
-        """Validates and sets the target_shape."""
-        if target_shape is None or isinstance(target_shape, _T.Sequence) and len(target_shape) == self._N:
-            self._target_shape = target_shape
-        else:
-            raise TypeError(f"invalid type or dimension of target_shape, must be None or {self._N}-sequence of ints, "
-                            f"got {target_shape}.")
-
-    def get_target_shape(self) -> _T.Optional[_T.Sequence[int]]:
+    @property
+    def target_shape(self) -> _T.Tuple[int, ...]:
         """Returns the target shape."""
         return self._target_shape
 
-    target_shape = property(get_target_shape, set_target_shape)
+    @target_shape.setter
+    def target_shape(self, target_shape: _T.Optional[_T.Sequence[int]]) -> None:
+        """Validates and sets the target_shape."""
+        tup_target_shape = tuple(target_shape) if isinstance(target_shape, _T.Iterable) else tuple()
+        if tup_target_shape != self._target_shape:
+            LOGGER.debug(f"Changing the target_shape of {type(self).__name__} to {tup_target_shape} from {self._target_shape}.")
+        elif (target_shape is None) != (self._target_shape is None):
+            LOGGER.debug(f"Changing the target_shape of {type(self).__name__} to {target_shape} from {self._target_shape}.")
+        if len(tup_target_shape) in [0, self._N]:
+            self._target_shape = tup_target_shape
+        else:
+            raise TypeError(f"invalid type or dimension of target_shape, must be None or {self._N}-sequence of ints, "
+                            f"got {target_shape}.")
 
     def forward(self,
                 input_tensor: Tensor,
@@ -88,8 +97,8 @@ class _ZoomNd(nn.Module):
         if input_tensor.dim() != 2 + self._N:
             raise ValueError("Expected {self._N+2}-dimensional input tensor, got {input.dim()}")
 
-        if self._target_shape is None:
-            raise AttributeError("The target_shape was None, but a value is required.")
+        if len(self._target_shape) == 0:
+            raise AttributeError("The target_shape was not set, but a valid value is required.")
 
         scales, aggregate = zip(*self._fix_scale_factors(scale_factors, input_tensor.shape[0]))
         scales, aggregate = list(scales), list(aggregate)
@@ -260,10 +269,9 @@ class Zoom2d(_ZoomNd):
         if crop_position not in ['top_left', 'bottom_left', 'top_right', 'bottom_right', 'center']:
             raise ValueError(f"invalid crop_position, got {crop_position}")
 
-        super(Zoom2d, self).__init__(None, interpolation_mode)
-        self._crop_position = crop_position
         self._N = 2
-        self.set_target_shape(target_shape)
+        super(Zoom2d, self).__init__(target_shape, interpolation_mode)
+        self._crop_position = crop_position
 
     def _interpolate(self, tensor: Tensor,
                      scale_factor: _T.Union[Tensor, np.ndarray, _T.Sequence[float]]) \
@@ -320,11 +328,13 @@ class Zoom3d(_ZoomNd):
     """
     Performs a crop and interpolation on a Five-dimensional Tensor respecting batch and channel.
     """
-    def __init__(self, target_shape: _T.Sequence[int], interpolation_mode: str="nearest", crop_position: str="front_top_left"):
+    def __init__(self, target_shape: _T.Optional[_T.Sequence[int]],
+                 interpolation_mode: str = "nearest",
+                 crop_position: str = "front_top_left"):
         """
         Initialization of Interpolation.
         Args:
-            target_shape (len 2): Target tensor size for after this module,
+            target_shape (len 3): Target tensor size for after this module,
                 not including batchsize and channels.
             interpolation_mode: interpolation mode as in `torch.nn.interpolate`
                 (default: 'neareast')
@@ -336,27 +346,31 @@ class Zoom3d(_ZoomNd):
             raise ValueError(f"invalid interpolation_mode, got {interpolation_mode}")
 
         if crop_position not in ['front_top_left', 'back_top_left',
-                'front_bottom_left', 'back_bottom_left', 'front_top_right', 'back_top_right',
-                'front_bottom_right', 'back_bottom_right', 'center']:
+                                 'front_bottom_left', 'back_bottom_left', 'front_top_right', 'back_top_right',
+                                 'front_bottom_right', 'back_bottom_right', 'center']:
             raise ValueError(f"invalid crop_position, got {crop_position}")
 
-        super(Zoom3d, self).__init__(None, interpolation_mode)
-        self._crop_position = crop_position
         self._N = 3
-        self.set_target_shape(target_shape)
+        super(Zoom3d, self).__init__(target_shape, interpolation_mode)
+        self._crop_position = crop_position
 
     def _interpolate(self, tensor: Tensor, scale_factor: _T.Sequence[int]):
         """
-        Crops, interpolates and pads the tensor acccording to the scale_factor. scale_factor must be 2-length sequence.
+        Crops, interpolates and pads the tensor acccording to the scale_factor. scale_factor must be 3-length sequence.
         """
-        if not isinstance(scale_factor, _T.Sequence) or len(scale_factor) != 3:
-            raise ValueError("target_shape must be a sequence of floats of length 3")
-
-        c = self._crop_position == "center"
-        depth_alignment, vertical_alignment, horizontal_alignment = ('from_start',) *3
+        scale_factor = scale_factor.tolist() if isinstance(scale_factor, np.ndarray) else scale_factor
+        if isinstance(scale_factor, Tensor) and scale_factor.shape == (3,):
+            pass
+        elif isinstance(scale_factor, _T.Sequence) and len(scale_factor) == 3:
+            scale_factor = list(scale_factor)
+        else:
+            raise ValueError(f"target_shape must be a ndarray, Tensor, or sequence of floats of length 3, received a "
+                             f"{type(scale_factor).__name__}: {scale_factor}.")
 
         if self._crop_position == "center":
-            depth_alignment, vertical_alignment, horizontal_alignment = ('mid',) *3
+            depth_alignment, vertical_alignment, horizontal_alignment = ('mid',) * 3
+        else:
+            depth_alignment, vertical_alignment, horizontal_alignment = ('from_start',) * 3
 
         if self._crop_position.startswith("back"):
             depth_alignment = 'from_end'

FastSurferCNN/run_prediction.py
@@ -14,6 +14,8 @@
 # limitations under the License.
 
 # IMPORTS
+from typing import Tuple, Optional, Union, Dict
+
 import numpy as np
 import torch
 import os
@@ -58,19 +60,26 @@ def set_up_cfgs(cfg, args):
 ##
 class RunModelOnData:
 
+    current_subject: str
+    subject_name: str
+    gt: Optional[nib.analyze.SpatialImage]
+    gt_data: Optional[np.ndarray]
+    orig: Optional[nib.analyze.SpatialImage]
+    orig_data: Optional[np.ndarray]
+
     def __init__(self, args):
-        self.current_subject = ""
-        self.subject_name = ""
-        self.gt, self.gt_data, self.orig, self.orig_data = "", "", "", ""
+        self.current_subject: str = ""
+        self.subject_name: str = ""
+        self.gt, self.gt_data, self.orig, self.orig_data = None, None, None, None
         self.sf = 1.0
         self.s = ""
         self.out_dir = args.out_dir
         self.orig_filename = os.path.join(self.current_subject, args.orig_name)
 
-        if args.gt_dir is None:
-            self.gt_filename = os.path.join(self.current_subject, args.gt_name)
-        else:
-            self.gt_filename = os.path.join(args.gt_dir, self.subject_name, args.gt_name)
+#        if args.gt_dir is None:
+#            self.gt_filename = os.path.join(self.current_subject, args.gt_name)
+#        else:
+#            self.gt_filename = os.path.join(args.gt_dir, self.subject_name, args.gt_name)
 
         if args.out_dir is not None:
             self.pred_name = os.path.join(args.out_dir, self.subject_name, args.pred_name)
@@ -160,28 +169,23 @@ class RunModelOnData:
         self.device = self.model.get_device()
         self.dim = self.model.get_max_size()
 
-    def run_model(self, pred_prob):
+    @torch.no_grad()
+    def run_model(self, out):
         # get prediction
-        pred_prob = self.model.run(self.orig_filename, self.orig_data, self.orig.header.get_zooms(),
-                                   pred_prob, noise=self.gn_noise)
+        return self.model.run(self.orig_filename, self.orig_data, self.orig.header.get_zooms(),
+                              out, noise=self.gn_noise)
 
-        # Post processing
-        h, w, d = self.orig_data.shape
-        pred_prob = pred_prob[0:h, 0:w, 0:d, :]
-
-        return pred_prob
-
-    def get_gt(self):
+    def get_gt(self) -> Tuple[nib.analyze.SpatialImage, np.ndarray]:
         return self.gt, self.gt_data
 
-    def get_orig(self):
+    def get_orig(self) -> Tuple[nib.analyze.SpatialImage, np.ndarray]:
         return self.orig, self.orig_data
 
     def get_prediction(self):
-        shape = self.orig.shape + (self.get_num_classes(),)
+        shape = self.orig.shape + (self.model.get_num_classes(),)
         kwargs = {
             "device": "cpu" if self.small_gpu else self.device,
-            "dtype": torch.float,
+            "dtype": torch.float16,  # TODO add a flag to choose between single and half precision?
             "requires_grad": False
         }
 
@@ -196,22 +200,26 @@ class RunModelOnData:
         # axial inference
         if self.view_ops["axial"]["cfg"] is not None:
             LOGGER.info("Run axial view agg")
+            self.set_model("axial")
             pred_prob = self.run_model(pred_prob)
 
         # sagittal inference
         if self.view_ops["sagittal"]["cfg"] is not None:
             LOGGER.info("Run sagittal view agg")
+            self.set_model("sagittal")
             pred_prob = self.run_model(pred_prob)
 
-        # Get hard predictions and map to freesurfer label space
-        pred_prob = torch.argmax(pred_prob, 3)
-        pred_prob = du.map_label2aparc_aseg(pred_prob, self.labels)
-        pred_prob = du.split_cortex_labels(pred_prob.cpu().numpy())
-        # return numpy array
-        return pred_prob
+        # Get hard predictions
+        pred_classes = torch.argmax(pred_prob, 3)
+        del pred_prob
+        # map to freesurfer label space
+        pred_classes = du.map_label2aparc_aseg(pred_classes, self.labels)
+        # return numpy array TODO: split_cortex_labels requires a numpy ndarry input
+        pred_classes = du.split_cortex_labels(pred_classes.cpu().numpy())
+        return pred_classes
 
     @staticmethod
-    def get_img(filename):
+    def get_img(filename: Union[str, os.PathLike]) -> Tuple[nib.analyze.SpatialImage, np.ndarray]:
         img = nib.load(filename)
         data = np.asanyarray(img.dataobj)
 
@@ -273,6 +281,26 @@ class RunModelOnData:
         return 79
 
 
+def handle_cuda_memory_exception(exception: RuntimeError, exit_on_out_of_memory: bool = True) -> bool:
+    if not isinstance(exception, RuntimeError):
+        return False
+    message = exception.args[0]
+    if message.startswith("CUDA out of memory. "):
+        LOGGER.critical("ERROR - INSUFFICIENT GPU MEMORY")
+        LOGGER.info("The memory requirements exceeds the available GPU memory, try using a smaller batch size "
+                    "(--batch_size <int>) and/or view aggregation on the cpu (--run_viewagg_on 'cpu')."
+                    "Note: View Aggregation on the GPU is particularly memory-hungry at approx. 5 GB for standard "
+                    "256x256x256 images.")
+        memory_message = message[message.find("(") + 1:message.find(")")]
+        LOGGER.info(f"Using {memory_message}.")
+        if exit_on_out_of_memory:
+            sys.exit("----------------------------\nERROR: INSUFFICIENT GPU MEMORY\n")
+        else:
+            return True
+    else:
+        return False
+
+
 if __name__ == "__main__":
     import argparse
 
@@ -281,8 +309,6 @@ if __name__ == "__main__":
     # 1. Options for input directories and filenames
     parser.add_argument("--in_dir", type=str, default=None,
                         help="Directory in which input volume(s) are located.")
-    parser.add_argument('--gt_name', type=str, default="mri/aseg.mgz",
-                        help="Default name for ground truth segmentations. Default: mri/aseg.filled.mgz")
     parser.add_argument('--orig_name', type=str, dest="orig_name", default='mri/orig.mgz', help="Name of orig input")
     parser.add_argument('--pred_name', type=str, dest='pred_name', default='mri/aparc.DKTatlas+aseg.deep.mgz',
                         help="Filename to save prediction. Default: mri/aparc.DKTatlas+aseg.deep.mgz")
@@ -325,6 +351,7 @@ if __name__ == "__main__":
     parser.add_argument("--cfg_sag", dest="cfg_sag", help="Path to the sagittal config file",
                         default=None, type=str)
 
+    parser.add_argument('--no_cuda', action='store_true', default=False, help="Disables GPU usage")
     parser.add_argument('--run_viewagg_on', dest='run_viewagg_on', type=str,
                         default="check", choices=["gpu", "cpu", "check"],
                         help="Define where the view aggregation should be run on. \
@@ -348,7 +375,7 @@ if __name__ == "__main__":
         sys.exit('----------------------------\nERROR: Please specify data output directory '
                  '(can be same as input directory)\n')
 
-    LOGGER.info("Ground truth: {}, Origs: {}".format(args.gt_name, args.orig_name))
+    LOGGER.info("Origs: {}".format(args.orig_name))
 
 
     # Set Up Model
@@ -372,8 +399,7 @@ if __name__ == "__main__":
             os.makedirs(args.out_dir)
 
         for subject in s_dirs:
-            # Set orig and gt for testing now
-            eval.set_gt(os.path.join(subject, args.gt_name))
+            # Set subject and orig
             eval.set_subject(subject)
             eval.set_orig(os.path.join(subject, args.orig_name))
             pred_name = os.path.join(args.out_dir, subject.strip('/'), args.pred_name)
@@ -381,7 +407,6 @@ if __name__ == "__main__":
             # Run model
             pred_data = eval.get_prediction()
 
-            gt, gt_data = eval.get_gt()
             if args.save_img:
                 eval.save_img(pred_name, pred_data, seg=True)
 
@@ -393,12 +418,17 @@ if __name__ == "__main__":
             os.makedirs(out_dir)
 
         # Set orig and gt for testing now
-        # Note: assumes orig_name, gt_name, and pred_name are absolute paths when --single_img:
+        # Note: assumes orig_name, and pred_name are absolute paths when --single_img:
         subject, img_name = os.path.split(args.orig_name)
         eval.set_subject(subject)
         eval.set_orig(args.orig_name)
 
         # Run model
-        pred_data = eval.get_prediction() #
-        if args.save_img:
+        try:
+            pred_data = eval.get_prediction()
+        except RuntimeError as e:
+            if not handle_cuda_memory_exception(e):
+                raise e
+
+        if args.save_img is not None:
             eval.save_img(args.pred_name, pred_data, seg=True)