addae61750dbff48d9cee37c2ca73518e8ded49f
David Kügler <david.kuegler@dzne.de>
2022-11-09 11:31:57 +0100

    optimize N4 bias field correction: - replace repeated percentiles (sorting) with single call - remove some of the repeated conversion between datatypes (unit/float32)

--- recon_surf/N4_bias_correct.py
+++ recon_surf/N4_bias_correct.py
@@ -18,12 +18,12 @@
 
 # IMPORTS
 import optparse
-import numpy as np
 import sys
-import SimpleITK as sitk
-import nibabel as nib
-import image_io as iio
 
+import SimpleITK as sitk
+import numpy as np
+
+import image_io as iio
 
 HELPTEXT = """
 
@@ -68,56 +68,74 @@ Original Author: Martin Reuter
 Date: Mar-18-2022
 """
 
-h_invol    = 'path to input.nii.gz'
-h_outvol   = 'path to corrected.nii.gz'
-h_mask     = 'optional: path to mask.nii.gz'
-h_shrink   = '<int> shrink factor, default: 4'
-h_levels   = '<int> number of fitting levels, default: 4'
-h_numiter  = '<int> max number of iterations per level, default: 50'
-h_thres    = '<float> convergence threshold, default: 0.0'
-h_skipwm   = 'skip normalize WM to 110 (for UCHAR scale)'
-h_tal      = '<sting> file name of talairach.xfm if using this for finding origin'
-h_threads  = '<int> number of threads, default: 1'
+h_invol = 'path to input.nii.gz'
+h_outvol = 'path to corrected.nii.gz'
+h_mask = 'optional: path to mask.nii.gz'
+h_shrink = '<int> shrink factor, default: 4'
+h_levels = '<int> number of fitting levels, default: 4'
+h_numiter = '<int> max number of iterations per level, default: 50'
+h_thres = '<float> convergence threshold, default: 0.0'
+h_skipwm = 'skip normalize WM to 110 (for UCHAR scale)'
+h_tal = '<sting> file name of talairach.xfm if using this for finding origin'
+h_threads = '<int> number of threads, default: 1'
+
 
 def options_parse():
     """
     Command line option parser
     """
-    parser = optparse.OptionParser(version='$Id: N4_bias_correct.py,v 1.0 2022/03/18 21:22:08 mreuter Exp $', usage=HELPTEXT)
-    parser.add_option('--in',      dest='invol',   help=h_invol)
-    parser.add_option('--out',     dest='outvol',  help=h_outvol)
-    parser.add_option('--mask',    dest='mask',    help=h_mask,    default=None)
-    parser.add_option('--shrink',  dest='shrink',  help=h_shrink,  default=4,     type="int")
-    parser.add_option('--levels',  dest='levels',  help=h_levels,  default=4,     type="int")
-    parser.add_option('--numiter', dest='numiter', help=h_numiter, default=50,    type="int")
-    parser.add_option('--thres',   dest='thres',   help=h_thres,   default=0.0,   type="float")
-    parser.add_option('--skipwm',  dest='skipwm',  help=h_skipwm,  default=False, action="store_true")
-    parser.add_option('--tal',     dest='tal',     help=h_tal,     default=None)
-    parser.add_option('--threads', dest='threads', help=h_threads, default=1,     type="int")
+    parser = optparse.OptionParser(version='$Id: N4_bias_correct.py,v 1.0 2022/03/18 21:22:08 mreuter Exp $',
+                                   usage=HELPTEXT)
+    parser.add_option('--in', dest='invol', help=h_invol)
+    parser.add_option('--out', dest='outvol', help=h_outvol)
+    parser.add_option('--mask', dest='mask', help=h_mask, default=None)
+    parser.add_option('--shrink', dest='shrink', help=h_shrink, default=4, type="int")
+    parser.add_option('--levels', dest='levels', help=h_levels, default=4, type="int")
+    parser.add_option('--numiter', dest='numiter', help=h_numiter, default=50, type="int")
+    parser.add_option('--thres', dest='thres', help=h_thres, default=0.0, type="float")
+    parser.add_option('--skipwm', dest='skipwm', help=h_skipwm, default=False, action="store_true")
+    parser.add_option('--tal', dest='tal', help=h_tal, default=None)
+    parser.add_option('--threads', dest='threads', help=h_threads, default=1, type="int")
     (options, args) = parser.parse_args()
 
     if options.invol is None or options.outvol is None:
-        sys.exit('\nERROR: Please specify --in input.nii --out output.nii as nifti\n   or use --help to see all options.\n')
+        sys.exit(
+            '\nERROR: Please specify --in input.nii --out output.nii as nifti\n   or use --help to see all options.\n')
 
     return options
 
 
 def N4correctITK(itkimage, itkmask=None, shrink=4, levels=4, numiter=50, thres=0.0, rescale=True):
+    """
+    Perform the bias field correction.
 
+    Args:
+        itkimage: itkImage[sitk.sitkFloat32]
+        itkmask: Optional[itkImage[sitk.sitk.Uint8]
+        shrink: Union[numbers.Rational]
+        levels: int
+        numiter: int
+        thres: float
+        rescale: bool
+
+    Returns: itkImage[sitk.sitkFloat32]
+    """
+
+    # if no mask is passed, create a simple mask from the image
     if not itkmask:
-        itkmask = itkimage>0
+        itkmask = itkimage > 0
         itkmask.CopyInformation(itkimage)
         print("- mask: default (>0)")
     else:
         # binarize mask
-        itkmask = itkmask>0
+        itkmask = itkmask > 0
 
     itkorig = itkimage
-    
+
     # subsample image
     if shrink > 1:
         itkimage = sitk.Shrink(itkimage, [shrink] * itkimage.GetDimension())
-        itkmask  = sitk.Shrink(itkmask,  [shrink] * itkimage.GetDimension())
+        itkmask = sitk.Shrink(itkmask, [shrink] * itkimage.GetDimension())
 
     # init corrector
     corrector = sitk.N4BiasFieldCorrectionImageFilter()
@@ -129,7 +147,7 @@ def N4correctITK(itkimage, itkmask=None, shrink=4, levels=4, numiter=50, thres=0
 
     # we need to apply bias field to original input
     log_bias_field = corrector.GetLogBiasFieldAsImage(itkorig)
-    itkcorrected = itkorig / sitk.Exp( log_bias_field )
+    itkcorrected = itkorig / sitk.Exp(log_bias_field)
 
     # normalize to average input intensity
     if rescale:
@@ -139,76 +157,83 @@ def N4correctITK(itkimage, itkmask=None, shrink=4, levels=4, numiter=50, thres=0
         stats.Execute(itkorig)
         m0 = stats.GetMean()
         print("- rescale")
-        print("   mean input: {:.4f} , mean corrected {:.4f} , image rescale: {:.4f}".format(m0,m1,m0/m1))
-        itkcorrected = itkcorrected * (m0/m1)
-    
-    itkcorrected = sitk.Cast(itkcorrected,sitk.sitkFloat32)
+        print("   mean input: {:.4f} , mean corrected {:.4f} , image rescale: {:.4f}".format(m0, m1, m0 / m1))
+        itkcorrected = itkcorrected * (m0 / m1)
+
+    # should already be Float 32
+    # itkcorrected = sitk.Cast(itkcorrected, sitk.sitkFloat32)
+    if itkimage.GetPixelIDTypeAsString() != "32-bit float":
+        print(f"The data type is: {itkimage.GetPixelIDTypeAsString()}")
     return itkcorrected
 
 
-def normalizeWM (itkimage, itkmask=None, radius=50, centroid=None, targetWM=110):
-    #print("\nnormalizeWM:")
-    
+def normalizeWM(itkimage, itkmask=None, radius=50, centroid=None, targetWM=110):
+    # print("\nnormalizeWM:")
+
     mask_passed = True
     if not itkmask:
         itkmask = sitk.OtsuThreshold(itkimage, 0, 1, 200)
         print("- mask: default (otsu)")
-        #itkmask = itkimage>0
-        #print("- mask: default (>0)")
+        # itkmask = itkimage>0
+        # print("- mask: default (>0)")
         itkmask.CopyInformation(itkimage)
         mask_passed = False
     else:
         # binarize mask
-        itkmask = itkmask>0
-    
+        itkmask = itkmask > 0
+
     if not centroid:
         # extract centroid from mask
         label_stats = sitk.LabelShapeStatisticsImageFilter()
         label_stats.Execute(itkmask)
-        #centroid_world = label_stats.GetCenterGravity(1)
+        # centroid_world = label_stats.GetCenterGravity(1)
         centroid_world = label_stats.GetCentroid(1)
-        #print("centroid pyhsical: {}".format(centroid_world))
-        #centroidf = itkmask.TransformPhysicalPointToContinuousIndex(centroid_world)
-        #print("centroid voxel: {}".format(centroidf))
-        centroid  = itkmask.TransformPhysicalPointToIndex(centroid_world)
+        # print("centroid pyhsical: {}".format(centroid_world))
+        # centroidf = itkmask.TransformPhysicalPointToContinuousIndex(centroid_world)
+        # print("centroid voxel: {}".format(centroidf))
+        centroid = itkmask.TransformPhysicalPointToIndex(centroid_world)
 
     print("- centroid: {}".format(centroid))
-    
+
     # create mask from ball around centroid with radius
     # create a spherical gaussian blob
     gaussian = sitk.GaussianSource(sitk.sitkUInt8,
-        size = itkimage.GetSize(),
-        sigma = [radius-1,radius-1,radius-1],
-        mean = centroid,
-        spacing = itkimage.GetSpacing())
+                                   size=itkimage.GetSize(),
+                                   sigma=[radius - 1, radius - 1, radius - 1],
+                                   mean=centroid,
+                                   spacing=itkimage.GetSpacing())
     # threshold to create a binary ball
     gaussian.CopyInformation(itkimage)
-    #sitk.WriteImage(gaussian, "gaussian.nii.gz")
+    # sitk.WriteImage(gaussian, "gaussian.nii.gz")
     ball = sitk.BinaryThreshold(gaussian, 150.0, 255.0, 1, 0)
     # make sure to crop non-brain regions (if masked was passed)
     #  warning do not use otsu mask as it is cropping low-intensity values
     if mask_passed:
-        ball = ball*itkmask
-    #sitk.WriteImage(ball, "ball.nii.gz")
+        ball = ball * itkmask
+    # sitk.WriteImage(ball, "ball.nii.gz")
 
     # get 1 and 90 percentiles of intensities in ball
     aimg = sitk.GetArrayFromImage(itkimage)
     amask = sitk.GetArrayFromImage(ball)
-    mask = np.extract(amask,aimg)
-    percentile90 = np.percentile(mask,90)
-    percentile01 = np.percentile(mask, 1)
-    print("-  1st percentile: {}\n- 90th percentile: {}".format(percentile01,percentile90))
+    mask = np.extract(amask, aimg)
+    percentiles = np.percentile(mask, [1, 90])
+    percentile01, percentile90 = percentiles.tolist()
+    print("-  1st percentile: {}\n- 90th percentile: {}".format(percentile01, percentile90))
 
     # compute intensity transformation
-    m = (targetWM - 2.55) / (percentile90 - percentile01) 
-    b = targetWM - m * percentile90 
-    print("- m: {:.4f}  b: {:.4f}".format(m,b))
+    m = (targetWM - 2.55) / (percentile90 - percentile01)
+    b = targetWM - m * percentile90
+    print("- m: {:.4f}  b: {:.4f}".format(m, b))
 
-    normed = sitk.Cast(sitk.Clamp(sitk.Cast(itkimage,sitk.sitkFloat32) * m + b,upperBound=255,lowerBound=0),sitk.sitkUInt8)
-    #sitk.WriteImage(normed, "normed.nii.gz")
+    # itkImage already is Float32 and output should be also Float32, we clamp outside as well
+    normed = itkimage * m + b
+
+#    normed = sitk.Cast(sitk.Clamp(sitk.Cast(itkimage, sitk.sitkFloat32) * m + b, upperBound=255, lowerBound=0),
+#                       sitk.sitkUInt8)
+    # sitk.WriteImage(normed, "normed.nii.gz")
 
     return normed
-    
+
 
 def readTalairachXFM(fname):
     with open(fname) as f:
@@ -217,22 +242,22 @@ def readTalairachXFM(fname):
     counter = 0
     tal = []
     for line in lines:
-        counter += 1 
-        firstword = line.split(sep="_",maxsplit=1)[0].lower()
+        counter += 1
+        firstword = line.split(sep="_", maxsplit=1)[0].lower()
         if firstword == "linear":
-            taltxt = np.char.replace(lines[counter:counter+3],';',' ')
+            taltxt = np.char.replace(lines[counter:counter + 3], ';', ' ')
             tal = np.genfromtxt(taltxt)
-            tal = np.vstack([tal, [0,0,0,1]])
-            #tal = np.loadtxt(fname,skiprows=counter,delimiter=)
-      
-    print(" tal: {}".format(tal))  
+            tal = np.vstack([tal, [0, 0, 0, 1]])
+            # tal = np.loadtxt(fname,skiprows=counter,delimiter=)
+
+    print(" tal: {}".format(tal))
     return tal
 
 
-def getTalOriginVox(tal,image):
+def getTalOriginVox(tal, image):
     talinv = np.linalg.inv(tal)
-    tal_origin = np.array(talinv[0:3,3]).ravel()
-    #print("Tal Physical Origin: {}".format(tal_origin))
+    tal_origin = np.array(talinv[0:3, 3]).ravel()
+    # print("Tal Physical Origin: {}".format(tal_origin))
     vox_origin = image.TransformPhysicalPointToIndex(tal_origin)
     print("Tal Voxel Origin: {}".format(vox_origin))
     return vox_origin
@@ -264,41 +289,37 @@ if __name__ == "__main__":
 
     # read image (only nii supported) and convert to float32
     print("\nreading {}".format(options.invol))
-    #itkimage = sitk.ReadImage(options.invol, sitk.sitkFloat32)
+    # itkimage = sitk.ReadImage(options.invol, sitk.sitkFloat32)
     itkimage, header = iio.readITKimage(options.invol, sitk.sitkFloat32, with_header=True)
 
     # if talaraich is passed
     talorig = None
     if options.tal:
         taltrans = readTalairachXFM(options.tal)
-        talorig = getTalOriginVox(taltrans,itkimage)
+        talorig = getTalOriginVox(taltrans, itkimage)
 
     # read mask (as uchar)
     if options.mask:
-        #itkmask = sitk.ReadImage(options.mask, sitk.sitkUInt8)
+        # itkmask = sitk.ReadImage(options.mask, sitk.sitkUInt8)
         itkmask = iio.readITKimage(options.mask, sitk.sitkUInt8)
     else:
-        itkmask=None
+        itkmask = None
 
     # call N4 correct
     print("\nexecuting N4 correction ...")
-    itkcorrected = N4correctITK(itkimage, itkmask, options.shrink, options.levels, options.numiter, options.thres, rescale=options.skipwm)
-    
+    itkcorrected = N4correctITK(itkimage, itkmask, options.shrink, options.levels, options.numiter, options.thres,
+                                rescale=options.skipwm)
+
     if not options.skipwm:
         print("\nnormalize WM to 110")
-        itkcorrected = normalizeWM (itkcorrected,itkmask,centroid=talorig)
-        print("\nconverting to UCHAR")
-        itkcorrected = sitk.Cast(sitk.Clamp(itkcorrected,lowerBound=0,upperBound=255),sitk.sitkUInt8)
+        itkcorrected = normalizeWM(itkcorrected, itkmask, centroid=talorig)
 
+    print("\nconverting to UCHAR")
+    itkcorrected = sitk.Cast(sitk.Clamp(itkcorrected, lowerBound=0, upperBound=255), sitk.sitkUInt8)
 
     # write image
     print("writing: {}".format(options.outvol))
-    #sitk.WriteImage(itkcorrected, options.outvol)
-    iio.writeITKimage(itkcorrected,options.outvol, header)
-    
+    # sitk.WriteImage(itkcorrected, options.outvol)
+    iio.writeITKimage(itkcorrected, options.outvol, header)
+
     sys.exit(0)
-
-
-
-
-
