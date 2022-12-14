fix wm scaling for highres

recon_surf/N4_bias_correct.py
@@ -188,34 +188,35 @@ def normalizeWM(itkimage, itkmask=None, radius=50, centroid=None, targetWM=110):
         label_stats.Execute(itkmask)
         # centroid_world = label_stats.GetCenterGravity(1)
         centroid_world = label_stats.GetCentroid(1)
-        # print("centroid pyhsical: {}".format(centroid_world))
-        # centroidf = itkmask.TransformPhysicalPointToContinuousIndex(centroid_world)
-        # print("centroid voxel: {}".format(centroidf))
+        #print("centroid pyhsical: {}".format(centroid_world))
+        #centroidf = itkmask.TransformPhysicalPointToContinuousIndex(centroid_world)
+        #print("centroid voxel: {}".format(centroidf))
         centroid = itkmask.TransformPhysicalPointToIndex(centroid_world)
 
     print("- centroid: {}".format(centroid))
+    print("- size: {}".format(itkimage.GetSize()))
+    print("- spacing: {}".format(itkimage.GetSpacing()))
 
-    # create mask from ball around centroid with radius
-    # create a spherical gaussian blob
-    gaussian = sitk.GaussianSource(sitk.sitkUInt8,
-                                   size=itkimage.GetSize(),
-                                   sigma=[radius - 1, radius - 1, radius - 1],
-                                   mean=centroid,
-                                   spacing=itkimage.GetSpacing())
-    # threshold to create a binary ball
-    gaussian.CopyInformation(itkimage)
-    # sitk.WriteImage(gaussian, "gaussian.nii.gz")
-    ball = sitk.BinaryThreshold(gaussian, 150.0, 255.0, 1, 0)
+    # distance image (never tested with anisotropic images, could fail)
+    isize = itkimage.GetSize()
+    ispace = itkimage.GetSpacing()
+    zz, yy, xx = np.meshgrid(range(isize[0]), range(isize[1]), range(isize[2]), indexing='ij')
+    xx = ispace[0] * (xx - centroid[0])
+    yy = ispace[1] * (yy - centroid[1])
+    zz = ispace[2] * (zz - centroid[2])
+    distance = xx * xx + yy * yy + zz * zz
+    ball = distance < radius * radius
+
     # make sure to crop non-brain regions (if masked was passed)
     #  warning do not use otsu mask as it is cropping low-intensity values
     if mask_passed:
-        ball = ball * itkmask
-    # sitk.WriteImage(ball, "ball.nii.gz")
+        ball = ball * sitk.GetArrayFromImage(itkmask)
+    #balli = sitk.GetImageFromArray(1.0 * ball)
+    #balli.CopyInformation(itkimage)
+    #sitk.WriteImage(balli, "ball.nii.gz")
 
     # get 1 and 90 percentiles of intensities in ball
-    aimg = sitk.GetArrayFromImage(itkimage)
-    amask = sitk.GetArrayFromImage(ball)
-    mask = np.extract(amask, aimg)
+    mask = np.extract(ball, sitk.GetArrayFromImage(itkimage))
     percentiles = np.percentile(mask, [1, 90])
     percentile01, percentile90 = percentiles.tolist()
     print("-  1st percentile: {}\n- 90th percentile: {}".format(percentile01, percentile90))
@@ -282,6 +283,8 @@ if __name__ == "__main__":
     print("- number iterations: {}".format(options.numiter))
     print("- convergence threshold: {}".format(options.thres))
     print("- skipwm: {}".format(bool(options.skipwm)))
+    if options.tal:
+        print("- talairach: {}".format(options.tal))
     print("- threads: {}".format(options.threads))
 
     # set number of threads
@@ -300,7 +303,6 @@ if __name__ == "__main__":
 
     # read mask (as uchar)
     if options.mask:
-        # itkmask = sitk.ReadImage(options.mask, sitk.sitkUInt8)
         itkmask = iio.readITKimage(options.mask, sitk.sitkUInt8)
     else:
         itkmask = None