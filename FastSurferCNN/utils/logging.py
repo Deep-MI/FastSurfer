0115b3b91c8a8cc66877727efd574501ee769ad1
ClePol <ClePol@users.noreply.github.com>
2023-02-15 14:51:03 +0100

    added catch for giving full filepath during inference

--- FastSurferCNN/utils/logging.py
+++ FastSurferCNN/utils/logging.py
@@ -20,15 +20,25 @@ from os import path, makedirs
 from sys import stdout as _stdout
 
 
-def setup_logging(output_dir: str, expr_num: str):
+def setup_logging(output: str, expr_num: str=None):
     """
     Sets up the logging
+    during trainnig output is a directory and expr_num is the experiment number
+    during inference output is a full file path and expr_num is None
+    TODO: resolve this dual purpose function
     """
     # Set up logging format.
     _FORMAT = "[%(levelname)s: %(filename)s: %(lineno)4d]: %(message)s"
-    log_folder = path.join(output_dir, "logs")
-    makedirs(log_folder, exist_ok=True)
-    log_file = path.join(log_folder, f"expr_{expr_num}.log")
+
+    if expr_num is not None: # training
+        assert(path.isdir(output)), f"Output directory {output} does not exist."
+        log_folder = path.join(output, "logs")
+        makedirs(log_folder, exist_ok=True)
+        log_file = path.join(log_folder, f"expr_{expr_num}.log")
+    elif expr_num is None and not path.isfile(output):
+        raise ValueError("A full full filepath during inference (i.e. when no experiment number is specified).")
+    else: # full file path given and no experiment number -> inference
+        log_file = output
 
     fh = FileHandler(filename=log_file, mode='a')
     ch = StreamHandler(_stdout)
