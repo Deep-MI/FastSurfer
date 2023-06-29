90427216a287bbad799a1f422c0ee9fde3918f3f
David Kügler <david.kuegler@dzne.de>
2023-06-29 13:52:21 +0200

    fix docker_build.sh write Version to console as well make log dir before making log add .gitignore

--- Docker/docker_build.sh
+++ Docker/docker_build.sh
@@ -3,19 +3,20 @@
 image_name="fastsurfer"
 image_tag="dev"
 
-pushd || exit
+pushd . || exit
 
-FASTSURFER_HOME="$(cd "$(dirname "$(dirname "$THIS_SCRIPT")")" &> /dev/null && pwd)"
+FASTSURFER_HOME="$(cd "$(dirname "$THIS_SCRIPT")" &> /dev/null && pwd)"
+FASTSURFER_HOME="$(cd "$(dirname "$FASTSURFER_HOME")" &> /dev/null && pwd)"
 export FASTSURFER_HOME
 version_tag="$(bash "$FASTSURFER_HOME/run_fastsurfer.sh" --version | cut -d" " -f 1)"
-bash "$FASTSURFER_HOME/run_fastsurfer.sh" --version long > "$FASTSURFER_HOME/VERSION.txt"
+bash "$FASTSURFER_HOME/run_fastsurfer.sh" --version long > "$FASTSURFER_HOME/BUILD.txt"
 echo "Version info added to the docker image:"
-cat "$FASTSURFER_HOME/VERSION.txt"
-docker build --rm=true -t "$image_name:$version_tag" -f ./Docker/Dockerfile .
+cat "$FASTSURFER_HOME/BUILD.txt"
+docker build --rm=true -t "$image_name:$version_tag" -f "$FASTSURFER_HOME/Docker/Dockerfile" "$FASTSURFER_HOME"
 if [ "$(docker image list "$image_name:$image_tag" | wc -l)" != "1" ]; then
   docker image rm $image_name:$image_tag
 fi
-docker tag "$image_name:$image_tag" "$image_name:$version_tag"
-rm "$FASTSURFER_HOME/VERSION.txt"
+docker tag "$image_name:$version_tag" "$image_name:$image_tag"
+rm "$FASTSURFER_HOME/BUILD.txt"
 
 popd || exit
