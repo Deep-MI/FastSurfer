290209851931b8817bdfc72eeff2559e1ef5930c
estradae <santiago.estrada@dzne.de>
2024-02-15 14:09:13 +0100

    Include Copyright statements
    Include copyright statements in Hypvinn files

--- HypVINN/models/networks.py
+++ HypVINN/models/networks.py
@@ -1,5 +1,5 @@
 
-# Copyright 2019 Image Analysis Lab, German Center for Neurodegenerative Diseases (DZNE), Bonn
+# Copyright 2024 AI in Medical Imaging, German Center for Neurodegenerative Diseases(DZNE), Bonn
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 
 
 # IMPORTS
