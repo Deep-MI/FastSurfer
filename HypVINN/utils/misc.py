1c202294d605f01668d752334aa3005c126f4bdd
santiestrada32 <sestradaleon@gmail.com>
2024-02-22 17:26:18 +0100

    Add Snapshot as optional argument

--- HypVINN/utils/misc.py
+++ HypVINN/utils/misc.py
@@ -1,10 +1,13 @@
 
-def create_expand_output_directory(root_path):
+def create_expand_output_directory(args):
     import os
+    root_path = args.out_dir
     paths = [root_path,
              os.path.join(root_path, 'mri', 'transforms'),
-             os.path.join(root_path, 'stats'),
-             os.path.join(root_path, 'qc_snapshots')]
+             os.path.join(root_path, 'stats')]
+    if args.qc_snapshots:
+        paths.append(os.path.join(root_path, 'qc_snapshots'))
+
     for path in paths:
         if path is not None and not os.path.exists(path):
             os.makedirs(path)
