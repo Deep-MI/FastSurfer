61754c7abef4327ee05602b9d06c17c563f3e3d2
Taha Abdullah <taha.abdullah20@gmail.com>
2024-03-18 17:34:00 +0100

    completed implementation of tests through command line arguments instead of yaml files

--- test/test_error_messages.py
+++ test/test_error_messages.py
@@ -4,11 +4,20 @@ import unittest
 import yaml
 
 class TestErrorMessages(unittest.TestCase):
-    
-    
+    """
+    A test case class to check for errors in the given files based on a YAML file.
+
+    This class defines test methods to verify if each file specified in the YAML file exists in the given folder.
+    """
     
     @classmethod
     def setUpClass(cls):
+        """
+        Set up the test class.
+
+        This method loads a YAML file, retrieves the log directory and error messages from the file,
+        and assigns them to class variables.
+        """
         try:
             # Load the YAML file
             with open(cls.error_file_path, 'r') as file:
@@ -21,11 +30,11 @@ class TestErrorMessages(unittest.TestCase):
             return
 
         # Get the folder path from the YAML file
-        cls.log_directory = data.get('log_path')
-        if not cls.log_directory:
-            print("The 'log_path' key was not found in the YAML file")
-            return
-
+        # cls.log_directory = data.get('log_path')
+        # if not cls.log_directory:
+        #     print("The 'log_path' key was not found in the YAML file")
+        #     return
+        cls.log_directory = ''
         # Get the error messages from the YAML file
         cls.errors = data.get('error_messages')
         if not cls.errors:
@@ -33,6 +42,12 @@ class TestErrorMessages(unittest.TestCase):
             return
 
     def test_find_errors_in_logs(self):
+        """
+        Test that the error messages are not in the log files.
+
+        This method retrieves the log files in the log directory, reads each log file,
+        and checks that each error message is not in the log file.
+        """
         if not self.log_directory or not self.errors:
             return
 
@@ -58,10 +73,21 @@ class TestErrorMessages(unittest.TestCase):
                 print(f"Error message '{error}' not found in {log_file}")
 
 if __name__ == '__main__':
-    
+    """
+    Main entry point of the script.
+
+    This block checks if there are any command line arguments, assigns the first argument to the error_file_path class variable,
+    and runs the unittest main function.
+    """
     if len(sys.argv) > 1:
+        TestErrorMessages.log_directory = sys.argv.pop()
         TestErrorMessages.error_file_path = sys.argv.pop()
+        
+        print('log directory: ' + TestErrorMessages.log_directory)
+        print('error file path: ' + TestErrorMessages.error_file_path)
+        
     else:
-        print("Please provide the path to the YAML file")
-    
+        print("Please provide error file and log directory")
+        test_args = sys.argv.pop()
+        print('test_args (else): ' + test_args)
     unittest.main()
--- test/test_file_existence.py
+++ test/test_file_existence.py
@@ -2,34 +2,45 @@ import os
 import sys
 import yaml
 import unittest
-import argparse
 
 class TestFileExistence(unittest.TestCase):
-    
-    # yaml_file_path = ''
-    
-    # if len(sys.argv) > 2:
-    #     yaml_file_path = sys.argv.pop()
-    # else:
-    #     print("Please provide the path to the YAML file")
+    """
+    A test case class to check the existence of files in a folder based on a YAML file.
+
+    This class defines test methods to verify if each file specified in the YAML file exists in the given folder.
+    """
     
     @classmethod
     def setUpClass(cls):
+        """
+        Set up the test case by loading the YAML file and extracting the folder path.
+
+        This method is executed once before any test methods in the class.
+        """
+        
         try:
             # Load the YAML file
-            with open(cls.yaml_file_path, 'r') as file:
+            with open(cls.file_path, 'r') as file:
                 cls.data = yaml.safe_load(file)
         except FileNotFoundError:
-            raise Exception(f"YAML file not found at path: {cls.yaml_file_path}")
+            raise Exception(f"YAML file not found at path: {cls.file_path}")
         except yaml.YAMLError as e:
             raise Exception(f"Error parsing YAML file: {e}")
 
         # Get the folder path from the YAML file
-        cls.folder_path = cls.data.get('folder_path')
-        if not cls.folder_path:
-            raise Exception("The 'folder_path' key was not found in the YAML file")
+        # cls.folder_path = cls.data.get('folder_path')
+        # if not cls.folder_path:
+        #     raise Exception("The 'folder_path' key was not found in the YAML file")
+        
+        cls.folder_path = ''
 
     def test_file_existence(self):
+        """
+        Test method to check the existence of files in the folder.
+
+        This method gets a list of all files in the folder recursively and checks if each file specified in the YAML file exists in the folder.
+        """
+        
         # Get a list of all files in the folder recursively
         filenames = []
         for root, dirs, files in os.walk(self.folder_path):
@@ -47,14 +58,21 @@ class TestFileExistence(unittest.TestCase):
             self.assertIn(file, filenames, f"File '{file}' does not exist in the folder.")
 
 if __name__ == '__main__':
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("yaml_file_path", help="Path to the YAML file")
-    # args = parser.parse_args()
-    
-    # TestFileExistence.yaml_file_path = sys.argv[2]
+    """
+    Main entry point of the script.
+
+    This block checks if there are any command line arguments, assigns the first argument to the error_file_path class variable,
+    and runs the unittest main function.
+    """
     
     if len(sys.argv) > 1:
-        TestFileExistence.yaml_file_path = sys.argv.pop()
+        TestFileExistence.folder_path = sys.argv.pop()
+        TestFileExistence.file_path = sys.argv.pop()
+        
+        print(f"Folder path: {TestFileExistence.folder_path}")
+        print(f"File path: {TestFileExistence.file_path}")
+        
+        
     else:
         print("Please provide the path to the YAML file")
         
