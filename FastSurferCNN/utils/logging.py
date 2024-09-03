63d5685d479f65bca3f72e51655daf9727bb3b54
Martin Reuter <reuter@mit.edu>
2024-09-03 22:38:21 +0200

    add back (locally) unused imports in logging

--- FastSurferCNN/utils/logging.py
+++ FastSurferCNN/utils/logging.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # IMPORTS
-from logging import INFO, FileHandler, StreamHandler, basicConfig
+from logging import getLogger, DEBUG, INFO, FileHandler, StreamHandler, basicConfig
 from pathlib import Path as _Path
 from sys import stdout as _stdout
 
