29033412236698245843f3ff1553eb27edd2563b
Taha Abdullah <taha.abdullah20@gmail.com>
2024-09-24 13:57:13 +0200

    Merging all functionality from CerebNet.utils.metrics.py into FastSurferCNN.utils.metrics.py - Deleting CerebNet.utils.metrics.py - refactoring usage across files - Changing back to numpy dice score implementation - removing CerebNet.utils.metrics.py from docs - Parameterized tests in test_images.py and test_stats.py - also added test configuration files for new filetypes - rebased pytest-feture onto merge-metrics - adding support for multiple images in test_images.py

--- FastSurferCNN/utils/metrics.py
+++ FastSurferCNN/utils/metrics.py
@@ -12,12 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any
-
-import numpy as np
 
 # IMPORTS
+import numpy as np
 import torch
+from scipy.ndimage import _ni_support
+from scipy.ndimage.morphology import (
+    binary_erosion,
+    distance_transform_edt,
+    generate_binary_structure,
+)
 
 from FastSurferCNN.utils import logging
 
@@ -103,175 +107,317 @@ def precision_recall(
 
 class DiceScore:
     """
-    Accumulate the component of the dice coefficient i.e. the union and intersection.
+    Accumulating the component of the dice coefficient i.e. the union and intersection.
 
-    Attributes
-    ----------
-    op : callable
-        A callable to update accumulator. Method's signature is `(accumulator, output)`.
-        For example, to compute arithmetic mean value, `op = lambda a, x: a + x`.
-    output_transform : callable, optional
-        A callable that is used to transform the
-        :class:`~ignite.engine.Engine`'s `process_function`'s output into the
-        form expected by the metric. This can be useful if, for example, you have a multi-output model and
-        you want to compute the metric with respect to one of the outputs.
-    device : str of torch.device, optional
-        Device specification in case of distributed computation usage.
-        In most of the cases, it can be defined as "cuda:local_rank" or "cuda"
-        if already set `torch.cuda.set_device(local_rank)`. By default, if a distributed process group is
-        initialized and available, device is set to `cuda`.
+    Args:
+        op (callable): a callable to update accumulator. Method's signature is `(accumulator, output)`.
+            For example, to compute arithmetic mean value, `op = lambda a, x: a + x`.
+        output_transform (callable, optional): a callable that is used to transform the
+            :class:`~ignite.engine.Engine`'s `process_function`'s output into the
+            form expected by the metric. This can be useful if, for example, you have a multi-output model and
+            you want to compute the metric with respect to one of the outputs.
+        device (str of torch.device, optional): device specification in case of distributed computation usage.
+            In most of the cases, it can be defined as "cuda:local_rank" or "cuda"
+            if already set `torch.cuda.set_device(local_rank)`. By default, if a distributed process group is
+            initialized and available, device is set to `cuda`.
     """
-
     def __init__(
         self,
-        num_classes: int,
-        device: str | None = None,
+        num_classes,
+        class_ids=None,
+        device=None,
+        one_hot=False,
         output_transform=lambda y_pred, y: (y_pred.data.max(1)[1], y),
     ):
-        """
-        Construct DiceScore object.
-        """
         self._device = device
         self.out_transform = output_transform
+        self.class_ids = class_ids
+        if self.class_ids is None:
+            self.class_ids = np.arange(num_classes)
         self.n_classes = num_classes
-        self.union = torch.zeros(self.n_classes, self.n_classes, device=device)
-        self.intersection = torch.zeros(self.n_classes, self.n_classes, device=device)
+        assert len(self.class_ids) == self.n_classes, (
+            f"Number of class ids is not correct,"
+            f" given {len(self.class_ids)} but {self.n_classes} is needed."
+        )
+        self.one_hot = one_hot
+        self.union = torch.zeros(self.n_classes, self.n_classes)
+        self.intersection = torch.zeros(self.n_classes, self.n_classes)
 
     def reset(self):
         """
-        Reset the union and intersection matrices to zero.
+        Reset the state of the object.
         """
-        self.union = torch.zeros(self.n_classes, self.n_classes, device=self._device)
-        self.intersection = torch.zeros(
-            self.n_classes, self.n_classes, device=self._device
-        )
+        self.union = torch.zeros(self.n_classes, self.n_classes)
+        self.intersection = torch.zeros(self.n_classes, self.n_classes)
 
     def _check_output_type(self, output):
         """
-        Check the output type.
+        Check the type of the output and raise an error if it doesn't match expectations.
+
+        Parameters:
+        -----------
+        output : tuple
+            The output to be checked, expected to be a tuple.
         """
         if not (isinstance(output, tuple)):
             raise TypeError(
-                f"Output should a tuple consist of of torch.Tensors, but given {type(output)}"
+                f"Output should be a tuple consisting of torch.Tensors, but given {type(output)}"
             )
 
-    def _update_union_intersection_matrix(
-        self, batch_output: torch.Tensor, labels_batch: torch.Tensor
-    ):
+    def _update_union_intersection(self, batch_output, labels_batch):
         """
-        Update the union intersection matrix.
+        Update the union and intersection matrices based on batch predictions and labels.
 
-        Parameters
-        ----------
+        Parameters:
+        -----------
         batch_output : torch.Tensor
-            Output tensor.
-        labels_batch : torch.Tensor
-            Label batch.
-        """
-        for i in range(self.n_classes):
-            gt = (labels_batch == i).float()
-            for j in range(self.n_classes):
-                pred = (batch_output == j).float()
-                self.intersection[i, j] += torch.sum(torch.mul(gt, pred))
-                self.union[i, j] += torch.sum(gt) + torch.sum(pred)
+            Batch predictions from the model.
 
-    def _update_union_intersection(
-        self, batch_output: torch.Tensor, labels_batch: torch.Tensor
-    ):
+        labels_batch : np.ndarray or torch.Tensor
+            Batch labels from the dataset.
         """
-        Update the union intersection.
+        # self.union.to(batch_output.device)
+        # self.intersection.to(batch_output.device)
+        for i, c1 in enumerate(self.class_ids):
+            gt = (labels_batch == c1).float()
+            for j, c2 in enumerate(self.class_ids):
+                pred = (batch_output == c2).float()
+                self.intersection[i, j] = self.intersection[i, j] + torch.sum(
+                    torch.mul(gt, pred)
+                )
+                self.union[i, j] = self.union[i, j] + torch.sum(gt) + torch.sum(pred)
 
-        Parameters
-        ----------
-        batch_output : torch.Tensor
-            Batch output (prediction, labels).
-        labels_batch : torch.Tensor
+    def update(self, output):
         """
-        for i in range(self.n_classes):
-            gt = (labels_batch == i).float()
-            pred = (batch_output == i).float()
-            self.intersection[i, i] += torch.sum(torch.mul(gt, pred))
-            self.union[i, i] += torch.sum(gt) + torch.sum(pred)
-
-    def update(self, output: tuple[Any, Any], cnf_mat: bool):
-        """
-        Update the intersection.
+        Update the internal state based on the output.
 
         Parameters
         ----------
-        output : Tuple[Any, Any]
-            Network output tensor.
-        cnf_mat : bool
-            Confusion matrix.
+        output : tuple of torch.Tensor
+            Tuple of predictions and labels.
         """
         self._check_output_type(output)
 
-        if self._device is not None:
-            # Put output to the metric's device
-            if isinstance(output, torch.Tensor) and (output.device != self._device):
-                output = output.to(self._device)
-        y_pred, y = self.out_transform(*output)
-
-        if cnf_mat:
-            self._update_union_intersection_matrix(y_pred, y)
+        if self.out_transform is not None:
+            y_pred, y = self.out_transform(*output)
         else:
-            self._update_union_intersection(y_pred, y)
+            y_pred, y = output
 
-    def compute_dsc(self) -> float:
+        if not isinstance(y, torch.Tensor):
+            y = torch.from_numpy(y)
+
+        if not isinstance(y_pred, torch.Tensor):
+            y_pred = torch.from_numpy(y_pred)
+
+        if self._device is not None:
+            y = y.to(self._device)
+            y_pred = y_pred.to(self._device)
+
+        self._update_union_intersection(y_pred, y)
+
+    def compute(self, per_class=False, class_idxs=None):
         """
-        Compute the dice score.
-
-        Returns
-        -------
-        dsc : float
-            Dice score.
+        Compute the Dice score.
         """
-        dsc_per_class = self._dice_calculation()
-        dsc = dsc_per_class.mean()
-        return dsc
+        dice_cm_mat = self._dice_confusion_matrix(class_idxs)
+        dice_score_per_class = dice_cm_mat.diagonal()
+        dice_score = dice_score_per_class.mean()
+        if per_class:
+            return dice_score_per_class, dice_cm_mat
+        else:
+            return dice_score, dice_cm_mat
 
-    def comput_dice_cnf(self):
+    def _dice_confusion_matrix(self, class_idxs):
         """
-        Compute the dice cnf.
-        """
-        dice_cm_mat = self._dice_confusion_matrix()
-        return dice_cm_mat
-
-    def _dice_calculation(self):
-        """
-        Calculate the Dice Score.
-
-        The Dice Score is calculated as 2 * (intersection / union).
-
-        Returns
-        -------
-        dsc : torch.Tensor
-            The Dice Score for each class.
-        """
-        intersection = self.intersection.diagonal()
-        union = self.union.diagonal()
-        dsc = 2 * torch.div(intersection, union)
-        return dsc
-
-    def _dice_confusion_matrix(self):
-        """
-        Calculate the Dice confusion matrix.
-
-        Returns
-        -------
-        dice_cnf_matrix : numpy.ndarray
-            The Dice confusion matrix for each class.
+        Compute the Dice score confusion matrix.
         """
         dice_intersection = self.intersection.cpu().numpy()
         dice_union = self.union.cpu().numpy()
+        if class_idxs is not None:
+            dice_union = dice_union[class_idxs[:, None], class_idxs]
+            dice_intersection = dice_intersection[class_idxs[:, None], class_idxs]
         if not (dice_union > 0).all():
             logger.info("Union of some classes are all zero")
         dice_cnf_matrix = 2 * np.divide(dice_intersection, dice_union)
         return dice_cnf_matrix
 
 
-def dice_score(cm):
+def dice_score(pred, gt, validate=True):
     """
-    [MISSING].
+    Calculates the Dice Similarity between pred and gt.
+
+    Parameters
+    ----------
+    pred : np.ndarray
+        Predicted image.
+    gt : np.ndarray
+        Ground truth image.
+    validate : bool
+        If True, use the scipy implementation of the Dice Similarity. If False, use the numpy implementation.
+
+    Returns
+    -------
+    float
+        Dice Similarity between pred and gt.
     """
-    pass
+    if validate:
+        from scipy.spatial.distance import dice
+        return dice(pred.flat, gt.flat)
+
+    else:
+        assert pred.dtype == gt.dtype == bool, "Input images must be boolean"
+
+        ntt = (pred & gt).sum()
+        nft = (~pred & gt).sum()
+        ntf = (pred & ~gt).sum()
+        return float((ntf + nft) / np.array(2.0 * ntt + ntf + nft))
+
+
+def volume_similarity(pred, gt):
+    """
+    Calculate the Volume Similarity between pred and gt.
+    """
+    pred_vol, gt_vol = np.sum(pred), np.save(gt)
+    return 1.0 - np.abs(pred_vol - gt_vol) / (pred_vol + gt_vol)
+
+
+# https://github.com/amanbasu/3d-prostate-segmentation/blob/master/metric_eval.py
+def hd(result, reference, voxelspacing=None, connectivity=1):
+    """
+    Computes the (symmetric) Hausdorff Distance (HD) between the binary objects in two
+    images. It is defined as the maximum surface distance between the objects.
+
+    Parameters
+    ----------
+    result : array_like
+        Input data containing objects. Can be any type but will be converted
+        into binary: background where 0, object everywhere else.
+    reference : array_like
+        Input data containing objects. Can be any type but will be converted
+        into binary: background where 0, object everywhere else.
+    voxelspacing : float or sequence of floats, optional
+        The voxelspacing in a distance unit i.e. spacing of elements
+        along each dimension. If a sequence, must be of length equal to
+        the input rank; if a single number, this is used for all axes. If
+        not specified, a grid spacing of unity is implied.
+    connectivity : int
+        The neighbourhood/connectivity considered when determining the surface
+        of the binary objects. This value is passed to
+        `scipy.ndimage.morphology.generate_binary_structure` and should usually be :math:`> 1`.
+        Note that the connectivity influences the result in the case of the Hausdorff distance.
+
+    Returns
+    -------
+    hd : float
+        The symmetric Hausdorff Distance between the object(s) in `result` and the
+        object(s) in `reference`. The distance unit is the same as for the spacing of
+        elements along each dimension, which is usually given in mm.
+    hd50 : float
+        The 50th percentile of the Hausdorff Distance.
+    hd95 : float
+        The 95th percentile of the Hausdorff Distance.
+
+    See Also
+    --------
+    assd : Average Symmetric Surface Distance, computes the average symmetric surface distance.
+    asd : Average Surface Distance, computes the average surface distance.
+
+    Notes
+    -----
+    This is a real metric. The binary images can therefore be supplied in any order.
+    """
+    hd1 = __surface_distances(result, reference, voxelspacing, connectivity)
+    hd2 = __surface_distances(reference, result, voxelspacing, connectivity)
+    hd = max(hd1.max(), hd2.max())
+    hd95 = np.percentile(np.hstack((hd1, hd2)), 95)
+    hd50 = np.percentile(np.hstack((hd1, hd2)), 50)
+    return hd, hd50, hd95
+
+
+def hd95(result, reference, voxelspacing=None, connectivity=1):
+    """
+    Computes the 95th percentile of the Hausdorff Distance.
+
+    Computes the 95th percentile of the (symmetric) Hausdorff Distance (HD) between the binary objects in two
+    images. Compared to the Hausdorff Distance, this metric is slightly more stable to small outliers and is
+    commonly used in Biomedical Segmentation challenges.
+
+    Parameters
+    ----------
+    result : Any
+        Input data containing objects. Can be any type but will be converted
+        into binary: background where 0, object everywhere else.
+    reference : array_like
+        Input data containing objects. Can be any type but will be converted
+        into binary: background where 0, object everywhere else.
+    voxelspacing : float or sequence of floats, optional
+        The voxelspacing in a distance unit i.e. spacing of elements
+        along each dimension. If a sequence, must be of length equal to
+        the input rank; if a single number, this is used for all axes. If
+        not specified, a grid spacing of unity is implied.
+    connectivity : int
+        The neighbourhood/connectivity considered when determining the surface
+        of the binary objects. This value is passed to
+        `scipy.ndimage.morphology.generate_binary_structure` and should usually be :math:`> 1`.
+        Note that the connectivity influences the result in the case of the Hausdorff distance.
+
+    Returns
+    -------
+    hd95 : float
+        The 95th percentile of the symmetric Hausdorff Distance between the object(s) in ```result``` and the
+        object(s) in ```reference```. The distance unit is the same as for the spacing of
+        elements along each dimension, which is usually given in mm.
+
+    See Also
+    --------
+    hd : Computes the symmetric Hausdorff Distance.
+
+    Notes
+    -----
+    This is a real metric. The binary images can therefore be supplied in any order.
+    """
+    hd1 = __surface_distances(result, reference, voxelspacing, connectivity)
+    hd2 = __surface_distances(reference, result, voxelspacing, connectivity)
+    hd95 = np.percentile(np.hstack((hd1, hd2)), 95)
+    return hd95
+
+
+def __surface_distances(result, reference, voxelspacing=None, connectivity=1):
+    """
+    The distances between the surface voxel of binary objects in result and their
+    nearest partner surface voxel of a binary object in reference.
+    """
+    result = np.atleast_1d(result.astype(np.bool))
+    reference = np.atleast_1d(reference.astype(np.bool))
+    if voxelspacing is not None:
+        voxelspacing = _ni_support._normalize_sequence(voxelspacing, result.ndim)
+        voxelspacing = np.asarray(voxelspacing, dtype=np.float64)
+        if not voxelspacing.flags.contiguous:
+            voxelspacing = voxelspacing.copy()
+
+    # binary structure
+    footprint = generate_binary_structure(result.ndim, connectivity)
+
+    # test for emptiness
+    if 0 == np.count_nonzero(result):
+        raise RuntimeError(
+            "The first supplied array does not contain any binary object."
+        )
+    if 0 == np.count_nonzero(reference):
+        raise RuntimeError(
+            "The second supplied array does not contain any binary object."
+        )
+
+    # extract only 1-pixel border line of objects
+    result_border = result ^ binary_erosion(result, structure=footprint, iterations=1)
+    reference_border = reference ^ binary_erosion(
+        reference, structure=footprint, iterations=1
+    )
+
+    # compute average surface distance
+    # Note: scipys distance transform is calculated only inside the borders of the
+    #       foreground objects, therefore the input has to be reversed
+    dt = distance_transform_edt(~reference_border, sampling=voxelspacing)
+    sds = dt[result_border]
+
+    return sds
