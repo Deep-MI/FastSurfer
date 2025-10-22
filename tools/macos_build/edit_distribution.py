6ef6be8818efbb1b84a8d8d6480660c6a949c330
Atabek Mykyev <otabekrintaro@gmail.com>
2025-10-22 16:33:00 +0200

    Fix script imports style

--- tools/macos_build/edit_distribution.py
+++ tools/macos_build/edit_distribution.py
@@ -11,9 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import sys
 import argparse
-
+import sys
 from pathlib import Path
 from xml.etree import ElementTree
 
