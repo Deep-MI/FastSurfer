51b7fff43c1d633d59c782cf2aad430fad53eccc
ClePol <ClePol@users.noreply.github.com>
2025-11-12 10:23:45 +0100

    added consolidation strategy with WM and ventricle labels

--- CorpusCallosum/paint_cc_into_pred.py
+++ CorpusCallosum/paint_cc_into_pred.py
@@ -21,6 +21,9 @@ import sys
 import nibabel as nib
 import numpy as np
 from numpy import typing as npt
+from scipy import ndimage
+
+from FastSurferCNN.data_loader.conform import is_conform
 
 HELPTEXT = """
 Script to add corpus callosum segmentation (CC, FreeSurfer IDs 251-255) to
@@ -74,7 +77,8 @@ def argument_parse():
     return args
 
 
-def paint_in_cc(pred: npt.NDArray[np.int_], aseg_cc: npt.NDArray[np.int_]) -> npt.NDArray[np.int_]:
+def paint_in_cc(pred: npt.NDArray[np.int_], 
+                aseg_cc: npt.NDArray[np.int_]) -> npt.NDArray[np.int_]:
     """Paint corpus callosum segmentation into aseg+dkt segmentation map.
 
     Parameters
@@ -98,21 +102,201 @@ def paint_in_cc(pred: npt.NDArray[np.int_], aseg_cc: npt.NDArray[np.int_]) -> np
     pred[cc_mask] = aseg_cc[cc_mask]
     return pred
 
+def correct_wm_ventricles(
+    aseg_cc: npt.NDArray[np.int_],
+    fornix_mask: npt.NDArray[np.bool_],
+    voxel_size: tuple[float, float, float],
+    close_gap_size_mm: float = 3.0
+) -> npt.NDArray[np.int_]:
+    """Correct WM mask and ventricle labels according to the CC and fornix masks.
+
+    The function
+    Take non-CC-connected WM components -> remove
+    Take FN -> WM
+    Fill space in superior inferior direction between CC and left/right Ventricle with corresponding Ventricle labels
+    """
+
+    # Create a copy to avoid modifying the original
+    corrected_pred = aseg_cc.copy()
+
+    # Get CC mask (labels 251-255)
+    cc_mask = (aseg_cc >= 251) & (aseg_cc <= 255)
+
+    # Get left and right ventricle masks
+    all_ventricle_mask = (aseg_cc == 4) | (aseg_cc == 43)
+
+    # Combine all WM labels
+    all_wm_mask = (aseg_cc == 2) | (aseg_cc == 41)
+
+
+    # 1. Fill space between CC and ventricles
+    # Only fill small gaps (up to 3 voxels) between CC and ventricle boundaries
+    #for ventricle_label, ventricle_mask in [(4, left_ventricle_mask), (43, right_ventricle_mask)]:
+    
+    # Process each slice independently
+    for x in range(corrected_pred.shape[0]):
+        cc_slice = cc_mask[x, :, :]
+        #vent_slice = ventricle_mask[x, :, :]
+        all_wm_slice = all_wm_mask[x, :, :]
+        
+
+        if all_wm_slice.any() and cc_slice.any():
+
+            # Dilate CC mask to find adjacent voxels, then check for overlap with component
+            cc_dilated = ndimage.binary_dilation(cc_slice, iterations=1)
+            # Label connected components in WM
+            labeled_wm, num_components = ndimage.label(all_wm_slice)
+
+            # Find components that are adjacent to CC and remove them
+            for label in range(1, num_components + 1):
+                component_mask = labeled_wm == label
+                # Check if this component is adjacent to (touches) the CC
+                if np.any(component_mask & cc_dilated):
+                    corrected_pred[x, :, :][component_mask] = 0  # Set to background
+
+
+            if fornix_mask[x, :, :].any():
+                fornix_slice = fornix_mask[x, :, :]
+                # count WM labels overlapping with fornix
+                left_wm_overlap = np.sum(fornix_slice & (aseg_cc == 2))
+                right_wm_overlap = np.sum(fornix_slice & (aseg_cc == 41))
+                if left_wm_overlap > right_wm_overlap:
+                    corrected_pred[x, :, :][fornix_slice] = 2  # Left WM
+                else:
+                    corrected_pred[x, :, :][fornix_slice] = 41  # Right WM    
+
+
+            vent_slice = all_ventricle_mask[x, :, :]
+
+            if cc_slice.any() and vent_slice.any():
+                # Create binary masks for this slice
+                cc_binary = cc_slice.astype(bool)
+                vent_binary = vent_slice.astype(bool)
+
+                # Dilate both masks slightly to find potential connection points
+                max_gap_vox = int(np.ceil(voxel_size[1] * close_gap_size_mm))
+                cc_dilated = ndimage.binary_dilation(cc_binary, iterations=max_gap_vox)
+                vent_dilated = ndimage.binary_dilation(vent_binary, iterations=max_gap_vox)
+
+                # Find voxels that are adjacent to both CC and ventricle but not part of either
+                potential_fill = (cc_dilated & vent_dilated) & ~(cc_binary | vent_binary)
+
+                # Only fill small gaps between CC and ventricle in inferior-superior direction
+                if potential_fill.any():
+
+
+                    for z in range(potential_fill.shape[1]):
+                        potential_fill_line = potential_fill[:, z]
+                        labeled_gaps, num_gaps = ndimage.label(potential_fill_line)
+                        cc_line = cc_binary[:, z]
+                        vent_line = vent_binary[:, z]
+
+
+                        
+                        
+
+                        for gap_label in range(1, num_gaps + 1):
+                            gap_mask = labeled_gaps == gap_label
+
+                            # check that CC and ventricle are connected to the gap_mask
+                            dilated_gap_mask = ndimage.binary_dilation(gap_mask, iterations=1)
+                            if not np.any(cc_line & dilated_gap_mask):
+                                continue
+                            if not np.any(vent_line & dilated_gap_mask):
+                                continue
+
+                            vent_label_location = np.where(vent_line & dilated_gap_mask)[0]
+                            vent_label = corrected_pred[x, vent_label_location, z]
+
+                            
+
+
+
+                            if np.sum(gap_mask) > max_gap_vox:
+                                continue
+
+                            corrected_pred[x, :, z][gap_mask  & (corrected_pred[x, :, z] == 0)] = vent_label
+
+                    # Process gaps in z-direction (within each y-row)
+                    for y in range(potential_fill.shape[0]):
+                        potential_fill_line = potential_fill[y, :]
+                        labeled_gaps, num_gaps = ndimage.label(potential_fill_line)
+                        cc_line = cc_binary[y, :]
+                        vent_line = vent_binary[y, :]
+
+                        for gap_label in range(1, num_gaps + 1):
+                            gap_mask = labeled_gaps == gap_label
+
+                            # check that CC and ventricle are connected to the gap_mask
+                            dilated_gap_mask = ndimage.binary_dilation(gap_mask, iterations=1)
+                            if not np.any(cc_line & dilated_gap_mask):
+                                continue
+                            if not np.any(vent_line & dilated_gap_mask):
+                                continue
+
+                            vent_label_location = np.where(vent_line & dilated_gap_mask)[0]
+                            if len(vent_label_location) > 0:
+                                vent_label = corrected_pred[x, y, vent_label_location[0]]  # Take first match
+
+                                if np.sum(gap_mask) > max_gap_vox:
+                                    continue
+
+                                corrected_pred[x, y, :][gap_mask  & (corrected_pred[x, y, :] == 0)] = vent_label
+
+
+    return corrected_pred
+
 
 if __name__ == "__main__":
     # Command Line options are error checking done here
     options = argument_parse()
 
+    
+
     print(f"Reading inputs: {options.input_cc} {options.input_pred}...")
-    aseg_image = np.asanyarray(nib.load(options.input_cc).dataobj)
-    prediction = nib.load(options.input_pred)
-    pred_with_cc = paint_in_cc(np.asanyarray(prediction.dataobj), aseg_image)
+    cc_seg_image = nib.load(options.input_cc)
+    cc_seg_data = np.asanyarray(cc_seg_image.dataobj)
+    aseg_image = nib.load(options.input_pred)
+    aseg_data = np.asanyarray(aseg_image.dataobj)
+
+    cc_conformed = is_conform(cc_seg_image, vox_size=None, img_size=None, verbose=False)
+    pred_conformed = is_conform(aseg_image, vox_size=None, img_size=None, verbose=False)    
+    if not cc_conformed:
+        print("Warning: CC input image is not conformed (LIA orientation, uint8 dtype). \
+              Please conform the image using the conform.py script.")
+    if not pred_conformed:
+        print("Warning: Prediction input image is not conformed (LIA orientation, uint8 dtype). \
+              Please conform the image using the conform.py script.")
+
+    # Count initial labels
+    initial_cc = np.sum((aseg_data >= 251) & (aseg_data <= 255))
+    initial_fornix = np.sum(aseg_data == 250)
+    initial_wm = np.sum((aseg_data == 2) | (aseg_data == 41))
+    print(f"Initial segmentation: CC={initial_cc}, Fornix={initial_fornix}, WM={initial_wm}")
+
+    # Paint CC into prediction
+    pred_with_cc = paint_in_cc(aseg_data, cc_seg_data)
+    after_paint_cc = np.sum((pred_with_cc >= 251) & (pred_with_cc <= 255))
+    print(f"After painting CC: {after_paint_cc} CC voxels added")
+
+    # Apply WM and ventricle corrections
+    print("Applying white matter and ventricle corrections...")
+    fornix_mask = cc_seg_data == 250
+    voxel_size = tuple(aseg_image.header.get_zooms())
+    pred_corrected = correct_wm_ventricles(aseg_data, fornix_mask, voxel_size)
+
+    # Count final labels
+    final_cc = np.sum((pred_corrected >= 251) & (pred_corrected <= 255))
+    final_fornix = np.sum(pred_corrected == 250)
+    final_wm = np.sum((pred_corrected == 2) | (pred_corrected == 41))
+    final_ventricles = np.sum((pred_corrected == 4) | (pred_corrected == 43))
+
+    print(f"Final segmentation: CC={final_cc}, Fornix={final_fornix}, WM={final_wm}, Ventricles={final_ventricles}")
+    print(f"Changes: CC +{final_cc-initial_cc}, Fornix {final_fornix-initial_fornix}, WM {final_wm-initial_wm}")
 
     print(f"Writing segmentation with corpus callosum to: {options.output}")
-    pred_with_cc_fin = nib.MGHImage(pred_with_cc, prediction.affine, prediction.header)
+    pred_with_cc_fin = nib.MGHImage(pred_corrected, aseg_image.affine, aseg_image.header)
     pred_with_cc_fin.to_filename(options.output)
 
     sys.exit(0)
 
-
-# TODO: Rename the file (paint_cc_into_asegdkt or similar) and functions.
