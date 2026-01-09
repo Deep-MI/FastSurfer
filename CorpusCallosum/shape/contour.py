bugfix in perimeter calculation

CorpusCallosum/shape/contour.py
@@ -78,7 +78,7 @@ class CCContour:
     def __init__(
         self,
         points: Points2dType,
-        thickness_values: np.ndarray[tuple[int], np.dtype[np.float_]] | None,
+        thickness_values: np.ndarray[tuple[int], np.dtype[np.float64]] | None,
         endpoint_idxs: tuple[int, int] | None = None,
         z_position: float = 0.0
     ):
@@ -740,7 +740,7 @@ class CCContour:
         contour: Points2dType,
         original_thickness_vertices: np.ndarray[tuple[int], np.dtype[np.signedinteger]] | None,
         input_path: str | Path,
-    ) -> np.ndarray[tuple[int], np.dtype[np.float_]]:
+    ) -> np.ndarray[tuple[int], np.dtype[np.float64]]:
         """See load_thickness_values.
 
         Ignore shape of thickness values if original_thickness_vertices is None.

CorpusCallosum/shape/subsegment_contour.py
@@ -100,11 +100,14 @@ def calc_subsegment_areas(split_contours: ContourList) -> np.ndarray[tuple[int],
     """
     # calculate area of each split contour using the shoelace formula
     # we use the absolute value because the orientation of the contour may vary
-    # support numpy <2 and >=2
-    if hasattr(np, 'trapezoid'):
-        areas_cum = np.abs([np.trapezoid(c[1], c[0]) for c in split_contours])
-    else:
-        areas_cum = np.abs([np.trapz(c[1], c[0]) for c in split_contours])
+    areas_cum = []
+    for c in split_contours:
+        # c is (2, N)
+        x = c[0]
+        y = c[1]
+        area = 0.5 * np.abs(np.sum(x * np.roll(y, -1) - np.roll(x, -1) * y))
+        areas_cum.append(area)
+    areas_cum = np.asarray(areas_cum)
 
     if len(areas_cum) == 1:
         return np.asarray(areas_cum[0])