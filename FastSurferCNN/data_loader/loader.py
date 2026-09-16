13d321ed7007162e2bfda1c3708dca7ee441295c
Martin Reuter <reuter@mit.edu>
2026-09-16 20:07:52 +0200

    Rename the lab in every copyright header

--- FastSurferCNN/data_loader/loader.py
+++ FastSurferCNN/data_loader/loader.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Image Analysis Lab, German Center for Neurodegenerative Diseases (DZNE), Bonn
+# Copyright 2019 DeepMI Lab, German Center for Neurodegenerative Diseases (DZNE), Bonn
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
